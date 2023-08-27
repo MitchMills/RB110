fddf212d03b31628a2e31a3394163d2da06a65dc
Mitch Mills <mitchmillsgcp@gmail.com>
2023-08-27 08:02:02 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -10,26 +10,99 @@ p minSubLength([1, 2, 4], 8) == 0
 =end
 
 
-
+p minSubLength([2, 3, 1, 2, 4, 3], 7) == 2
+p minSubLength([1, 10, 5, 2, 7], 9) == 1
+p minSubLength([1, 11, 100, 1, 0, 200, 3, 2, 1, 250], 280) == 4
+p minSubLength([1, 2, 4], 8) == 0
 
 
 
 
 
 #### RACHELE II, PROBLEM 1
-# Write a method that takes an array of consecutive letters as input and returns the missing letter.
+=begin
+Write a method that takes an array of consecutive letters as input and returns the missing letter.
+
+p determine_missing_letter(['a','b','c','d','f']) == 'E' 
+p determine_missing_letter(['o','q','r','s']) == 'P'
+p determine_missing_letter(['H','J','K','L']) == 'i'
+p determine_missing_letter([]) == []
+
+PROBLEM__________
+Input: array
+  - each element is a string
+  - each string is a single alphabetic character
+  - the elements are in a sequence
+  - the sequence is alphabetic, ascending
+  - all elements are in the same case
+  - a letter is missing from somewhere in the middle of the sequence
+  - array can be empty
+
+Output: string OR empty array
+  - string is a single alphabetic character
+  - it represents the missing letter in the sequence
+  - it is in the opposite case from the input array
+  - if input array is empty, it is returned instead of a string
+
+Rules
+- Explicit
+  - input array contains consecutive letters
+  - output is the missing letter from sequence
+- Implicit
+  - input array is in an alphabetical sequence
+  - one letter is missing from the middle of the sequence
+  - letters in input array are all the same case
+  - output letter must be in the opposite case
+  - if the input array is empty, return an empty array instead of a string
+
+EXAMPLES__________
+p determine_missing_letter(['a','b','c','d','f']) == 'E' 
+p determine_missing_letter(['o','q','r','s']) == 'P'
+p determine_missing_letter(['H','J','K','L']) == 'i'
+p determine_missing_letter([]) == []
+
+DATA STRUCTURES__________
+input: array # ['a','b','c','d','f']
+
+array of complete sequence # ['a','b','c','d','e','f']
+missing letter # 'e'
+
+output: string # 'E'
+        empty array (if input array is empty)
+
+ALGORITHM__________
+- Return input array if it is empty
+
+- Create an array containing the full sequence
+  - (array[0]..array[-1])
+
+- Determine the missing letter
+  - remove all input array letters from the full sequence
+
+- Return the missing letter
+  - get missing letter string
+  - swap the case
+  - return it
+=end
+
+# def determine_missing_letter(sequence)
+#   return sequence if sequence.empty?
+
+#   full_sequence = (sequence[0]..sequence[-1]).to_a
+
+#   missing_letter = full_sequence - sequence
+
+#   missing_letter.first.swapcase
+# end
 
 # p determine_missing_letter(['a','b','c','d','f']) == 'E' 
 # p determine_missing_letter(['o','q','r','s']) == 'P'
 # p determine_missing_letter(['H','J','K','L']) == 'i'
 # p determine_missing_letter([]) == []
 
-
-
-
-
 ############### RACHELE SESSION I
 #### RACHELE I, PROBLEM 2
+=begin
 # For "x", determine how many positive integers less than or equal to "x" are odd but not prime. Assume "x" is an integer between 1 and 10000.
 
 # Example: 5 has three odd numbers (1,3,5) and only the number 1 is not prime, so the answer is 1
@@ -37,10 +110,80 @@ p minSubLength([1, 2, 4], 8) == 0
 
 # A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
-p odd_not_prime(15) == 3
+**PROBLEM___________________
+- for a given number, find the number of positive integers that are:
+  - less than OR EQUAL to it &&
+  - odd &&
+  - not prime
+
+*Input: integer
+  - integer must be positive (there are no positive integers less than or equal to a negative number or to 0)
+
+*Output: integer
+  - represents the number of positive integers that meet all three criteria
+
+*Rules:
+  - Explicit
+    - target numbers must meet all three criteria:
+      - <= to input integer
+      - odd
+      - not prime
+    - Prime number: integer greater than 1 that has no positive divisors besides 1 and itself
+  - Implicit
+    - input integer is positive
+    - all input integers have at least one odd non-prime number less than or equal to them: 1
+
+Questions:
+- do we need to account for input integers that are not positive? I.e. return 0 (probably)
+  - Executive Decision: NO
+
+**EXAMPLES___________________
+p odd_not_prime(15) == 3 # [1, 3, 5, 7, 9, 11, 13, 15] --> [1, 9, 15]
 p odd_not_prime(48) == 10
 p odd_not_prime(82) == 20
 
+**DATA STRUCTURES___________________
+input: integer # 15
+
+array of all odd numbers <= input integer # [1, 3, 5, 7, 9, 11, 13, 15]
+array of all of those numbers which are not prime # [1, 9, 15]
+
+output: integer # 3
+
+**ALGORITHM___________________
+- Get all odd numbers between 1 and input integer
+  - create a range from 1 up to the input integer
+  - select all odd numbers from that range
+  - put those nubmers into an array: `odd_numbers`
+
+- Select non-prime numbers
+  - create an array `odd_non_primes` containing the number 1: [1]
+  - iterate over `odd_numbers`
+    - for each `number` in `odd_numbers`
+      -iterate each `divisor` between 3 and (`number` - 1)
+        - (we've accounted for 1, and 2 only divides evenly into even numbers)
+        - (`number` will always divide evenly into itself)
+        - if any `divisor` divides evenly (% == 0) into `number`, add `number` to `odd_non_primes`
+
+- Return result
+  - the number of unique numbers in `odd_non_primes`
+=end
+
+# def odd_not_prime(number)
+#   odd_numbers = (1..number).select { |number| number.odd? }
+
+#   odd_non_primes = [1]
+#   odd_numbers.each do |number|
+#     (3..(number - 1)).step(2).each do |divisor|
+#       odd_non_primes << number if number % divisor == 0
+#     end
+#   end
+#   odd_non_primes.uniq.size
+# end
+
+# p odd_not_prime(15) == 3
+# p odd_not_prime(48) == 10
+# p odd_not_prime(82) == 20
 
 
 
