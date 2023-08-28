f16f17988d8ba21b21ec03e6eaf4f96e9a3f52e4
Mitch Mills <mitchmillsgcp@gmail.com>
2023-08-28 15:32:07 -0500

    Reworked easy 6 exercises

--- 00Practice/Small_Problems_Exercises/easy6.rb
+++ 00Practice/Small_Problems_Exercises/easy6.rb
@@ -1,12 +1,39 @@
-### 7.2 HALVSIES
-def halvsies(array)
-
+### 8.2 FIND THE DUPLICATE
+def find_dup(array)
+  counts = array.tally
+  counts.rassoc(2).first
 end
 
-p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
-p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
-p halvsies([5]) == [[5], []]
-p halvsies([]) == [[], []]
+p find_dup([1, 5, 1, 3])# == 1
+p find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+          7,  34, 57, 74, 45, 11, 88, 67,  5, 58])# == 73
+
+### 7.2 HALVSIES
+# def halvsies(array)
+#   midpoint = array.size / 2
+#   front_slice_size = array.size - midpoint
+
+#   front_slice = array[0, front_slice_size]
+#   back_slice = array - front_slice
+#   [front_slice, back_slice]
+# end
+
+# array = [1, 2, 3, 4, 5]
+# p halvsies(array)
+# p array
+
+# p halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+# p halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+# p halvsies([5]) == [[5], []]
+# p halvsies([]) == [[], []]
 
 ### 6.2 COMBINING ARRAYS
 # def merge(array_1, array_2) # non-mutating
