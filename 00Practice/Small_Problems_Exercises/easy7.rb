a36d662ed0610798cd85a2d35159b84b6086c2be
Mitch Mills <mitchmillsgcp@gmail.com>
2023-08-31 14:01:26 -0500

    Ran code snippets

--- 00Practice/Small_Problems_Exercises/easy7.rb
+++ 00Practice/Small_Problems_Exercises/easy7.rb
@@ -1,12 +1,12 @@
 ### 2.2 
-def letter_case_count(string)
+# def letter_case_count(string)
 
-end
+# end
 
-p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
-p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
-p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
-p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+# p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+# p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+# p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+# p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
 
 ### 1.2 COMBINE TWO LISTS
 # def interleave(array1, array2)
