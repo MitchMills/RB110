6ce071ee70ec3686ceca8aaafbb117bf3d24dde8
Mitch Mills <mitchmillsgcp@gmail.com>
2023-09-01 15:49:20 -0500

    PEDAC practice

--- 02Advanced_Ruby_Collections/pedac_scratchpad.md
+++ 02Advanced_Ruby_Collections/pedac_scratchpad.md
@@ -1,7 +1,17 @@
 # PROBLEM
-Have the method letter_changes(str) take the str parameter being passed and modify it using the following algorithm:
-  - Replace every letter in the string with the 3rd letter following it in the alphabet (ie. c becomes f, Z becomes C).
-  - Then return this modified string.
+Have the method letter_changes(str) take the str parameter being passed and
+modify it using the following algorithm:
+  -Replace every letter in the string with the 3rd letter following it in the alphabet (ie. c becomes f, Z becomes C).
+Then return this modified string.
+
+input: string
+  - can contain letters, non-letter characters, spaces, upper and lower case
+
+output: string
+  - every letter in input string replaced with 3rd letter following it
+  - non-letter characters are unchanged
+  - case is preserved
+  - if end of alphabet is reached, loop back around: x -> a, y -> b, z -> c
 
 # EXAMPLES
 p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
@@ -10,9 +20,23 @@ p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
 p letter_changes('xyz') == ('abc')
 
 # DATA STRUCTURE(S)
+input: string
+  - array of each character
+  - array of alphabet: upper and lower
+  - array of alphabet shifted 3 places: upper and lower
 
+output: string
 
 # ALGORITHM
+- create two arrays: uppercase alphabet, lowercase alphabet
+- create two arrays: shifted uppercase alphabet, shifted lowercase alphabet
+
+- separate the input string into an array of letters
+- iterate over this array
+  - for each char, if it is included in one of the alphabets, get the index of that character in the alphabet, and replace it with the character at the same index in the shifted alphabet
+  - otherwise leave the character unchanged
+- join the array back into a string
+- return the string
 
 
 #################################
--- 02Advanced_Ruby_Collections/sandbox.rb
+++ 02Advanced_Ruby_Collections/sandbox.rb
@@ -1,5 +1,37 @@
 def letter_changes(string)
+  uppercase = ('A'..'Z').to_a
+  lowercase = ('a'..'z').to_a
+  shifted_uppercase = ('D'..'Z').to_a + ('A'..'C').to_a
+  shifted_lowercase = ('d'..'z').to_a + ('a'..'c').to_a
+  string.chars.map do |char|
+    if uppercase.include?(char)
+      index = uppercase.index(char)
+      shifted_uppercase[index]
+    elsif lowercase.include?(char)
+      index = lowercase.index(char)
+      shifted_lowercase[index]
+    else
+      char
+    end
+  end.join
+end
 
+
+
+def letter_changes(string)
+  uppercase = ('A'..'Z').to_a
+  lowercase = ('a'..'z').to_a
+  string.chars.map do |char|
+    if uppercase.include?(char)
+      index = uppercase.index(char)
+      uppercase.rotate(3)[index]
+    elsif lowercase.include?(char)
+      index = lowercase.index(char)
+      lowercase.rotate(3)[index]
+    else
+      char
+    end
+  end.join
 end
 
 
@@ -18,6 +50,8 @@ p letter_changes('xyz') == ('abc')
 #       string[start_index, length]
 #     end
 #   end.flatten
+
+#   p all_substrings
   
 #   vowel_only_substrings = all_substrings.select do |substring|
 #     substring.chars.all? do |char|
@@ -32,6 +66,7 @@ p letter_changes('xyz') == ('abc')
 #   end.size
 # end
 
+### With Helper Methods
 # VOWELS = %w(a e i o u)
 
 # def count_vowel_substrings(string)
@@ -72,8 +107,82 @@ p letter_changes('xyz') == ('abc')
 #     end
 #   end
 # end
+###
+### ESTHER I
+# def count_vowel_substrings(string)
+#   counter = 0
+
+#   5.upto(string.size) do |length|
+#     string.chars.each_cons(length) do |chars_arr|
+#       all_vowels = chars_arr.all? { |char| %w(a e i o u).include?(char) }
+#       counter += 1 if all_vowels && chars_arr.uniq.size == 5
+#     end
+#   end
+
+#   counter
+# end
+###
+
+### ESTHER II
+# def count_vowel_substrings(string)
+#   counter = 0
+
+#   0.upto(string.size - 5) do |start_idx|
+#     5.upto(string[start_idx..-1].size) do |length|
+#       substring = string[start_idx, length]
+#       all_vowels = substring.chars.all? { |char| %w(a e i o u).include?(char) }
+#       uniq_chars = substring.chars.sort.join.squeeze
+#       counter += 1 if all_vowels && uniq_chars.size == 5
+#     end
+#   end
+
+#   counter
+# end
+###
+
+### ESTHER III
+# def count_vowel_substrings(string)
+#   vowel_substrings = []
+
+#   0.upto(string.size - 5) do |start_idx|
+#     current_idx = start_idx
+
+#     while current_idx < string.size
+#       break unless %w(a e i o u).include?(string[current_idx])
+
+#       substring = string[start_idx..current_idx]
+#       vowel_substrings << substring if substring.chars.uniq.size == 5
+#       current_idx += 1
+#     end
+
+#     next
+#   end
+
+#   vowel_substrings.size
+# end
+###
+
+### SHAWN
+# def count_vowel_substrings(string)
+#   substring_array = []
+#   0.upto(string.size - 1) do |outer_idx|
+#     outer_idx.upto(string.size - 1) do |inner_idx|
+
+#       if %w(a e i o u).all? { |vowel| string[outer_idx..inner_idx].count(vowel) >= 1 }
+#         if string[outer_idx..inner_idx].delete('^aeiou') == string[outer_idx..inner_idx]
+#           substring_array << string[outer_idx..inner_idx]
+#         end
+#       end
+
+#     end
+#   end
+  
+#   substring_array.size
+# end
 
 # p count_vowel_substrings("abcde") == 0
 # p count_vowel_substrings("cuaieuouac") == 7
 # p count_vowel_substrings("aeiouu") == 2
 # p count_vowel_substrings("unicornarihan") == 0
+
+
