9f0d6df4f4fb2098de300a45b5b06a9ee18a97b6
Mitch Mills <mitchmillsgcp@gmail.com>
2023-09-01 10:48:23 -0500

    PEDAC practice

--- 02Advanced_Ruby_Collections/sandbox.rb
+++ 02Advanced_Ruby_Collections/sandbox.rb
@@ -1,3 +1,27 @@
+# def count_vowel_substrings(string)
+#   vowels = %w(a e i o u)
+#   non_vowels = ('a'..'z').to_a - vowels
+
+#   all_substrings = (0..string.size - 5).map do |start_index|
+#     next [] if non_vowels.include?(string[start_index])
+#     (5..string.size - start_index).map do |length|
+#       string[start_index, length]
+#     end
+#   end.flatten
+  
+#   vowel_only_substrings = all_substrings.select do |substring|
+#     substring.chars.all? do |char|
+#         vowels.include?(char)
+#     end
+#   end
+
+#   vowel_substrings = vowel_only_substrings.select do |substring|
+#     vowels.all? do |vowel|
+#         substring.include?(vowel)
+#     end
+#   end.size
+# end
+
 def count_vowel_substrings(string)
   vowels = %w(a e i o u)
   non_vowels = ('a'..'z').to_a - vowels
@@ -7,33 +31,27 @@ def count_vowel_substrings(string)
     (5..string.size - start_index).map do |length|
       string[start_index, length]
     end
-  end
+  end.flatten
   
-  vowel_only_substrings = all_substrings.select do |array|
-    array.select do |string|
-
-      result = string.chars.all? do |char|
+  vowel_only_substrings = all_substrings.select do |substring|
+    substring.chars.all? do |char|
         vowels.include?(char)
-      end
-      p result
     end
-    array.size > 0
   end
-  p vowel_only_substrings
+
+  vowel_substrings = vowel_only_substrings.select do |substring|
+    vowels.all? do |vowel|
+        substring.include?(vowel)
+    end
+  end.size
 end
 
 
 
-# [
-#   [], 
-#   ["uaieu", "uaieuo", "uaieuou", "uaieuoua", "uaieuouac"], 
-#   ["aieuo", "aieuou", "aieuoua", "aieuouac"], 
-#   ["ieuou", "ieuoua", "ieuouac"], 
-#   ["euoua", "euouac"], 
-#   ["uouac"]
-# ]
 
-# p count_vowel_substrings("abcde") == 0
-count_vowel_substrings("cuaieuouac")# == 7
-# p count_vowel_substrings("aeiouu") == 2
-# p count_vowel_substrings("unicornarihan") == 0
+
+
+p count_vowel_substrings("abcde") == 0
+p count_vowel_substrings("cuaieuouac") == 7
+p count_vowel_substrings("aeiouu") == 2
+p count_vowel_substrings("unicornarihan") == 0
