1669e76d6823bf73eff34400a4d94b4cbcf4b5b7
Mitch Mills <mitchmillsgcp@gmail.com>
2023-09-01 11:40:25 -0500

    PEDAC practice

--- 02Advanced_Ruby_Collections/pedac_scratchpad.md
+++ 02Advanced_Ruby_Collections/pedac_scratchpad.md
@@ -1,72 +1,18 @@
 # PROBLEM
-A substring is a contiguous (non-empty) sequence of characters within a string.
-  
-A vowel substring is a substring that only consists of vowels ('a', 'e', 'i', 'o', and 'u') and has all five vowels present in it.
-  
-Given a string word, return the number of vowel substrings in word.
-
-
-- input: string
-  - one "word": no spaces
-  - all lowercase
-
-- output: integer
-  - represents the number of vowel substrings in the input string
-  - substring: contiguous sequence of characters within a string
-  - vowel substring:
-    - contains only vowels (aeiou)
-    - has all five vowels present
-    - order doesn't matter
-    - case doesn't matter
-    - can be entire input string, i.e. same length as input string
-    - must be at least 5 characters in length
-    - a given character can be part of multiple vowel substrings
+Have the method letter_changes(str) take the str parameter being passed and modify it using the following algorithm:
+  - Replace every letter in the string with the 3rd letter following it in the alphabet (ie. c becomes f, Z becomes C).
+  - Then return this modified string.
 
 # EXAMPLES
-p count_vowel_substrings("abcde") == 0
-p count_vowel_substrings("cuaieuouac") == 7
-p count_vowel_substrings("aeiouu") == 2
-p count_vowel_substrings("unicornarihan") == 0
-
-cuaieuouac:
-  - uaieuo
-  - uaieuou
-  - uaieuoua
-  - aieuo
-  - aieuou
-  - aieuoua
-  - ieuoua
+p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
+p letter_changes("Road trip9") == "Urdg wuls9"
+p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
+p letter_changes('xyz') == ('abc')
 
 # DATA STRUCTURE(S)
-- string
-  - array to hold all possible substrings
-  - array to all successful substrings
--integer
 
-# NOTES
-- minimum string length is 5:
-  - if string is length 10, last string at least 5 characters long starts at index 5: 0 1 2 3 4   5 6 7 8 9
-  - so start index range is: 0..(string.length - 5)
-
-- string length:
-  - must be at least 5
-  - can be entire input string
-  - depends on starting index
-  - length range: 5..(string.length - current starting index)
 
 # ALGORITHM
-- find all possible substrings
-  - start at 0 index and find all substrings, then go to next index . . .
-    - start at index 0, go until index (input string length - 5)
-
-    - at a given index, get all possible lengths from that starting point
-      - length 5, then length 6, ...
-        - start at length 5, go until length (input string length - current index)
-    - put all substrings into an array
-
-- filter out unwanted substrings
-- count number of substrings left
-- return count
 
 
 #################################
--- 02Advanced_Ruby_Collections/sandbox.rb
+++ 02Advanced_Ruby_Collections/sandbox.rb
@@ -1,9 +1,19 @@
-# def count_vowel_substrings(string)
-#   vowels = %w(a e i o u)
-#   non_vowels = ('a'..'z').to_a - vowels
+def letter_changes(string)
 
+end
+
+
+p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
+p letter_changes("Road trip9") == "Urdg wuls9"
+p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
+p letter_changes('xyz') == ('abc')
+
+
+##### VOWEL SUBSTRINGS
+# VOWELS = %w(a e i o u)
+
+# def count_vowel_substrings(string)
 #   all_substrings = (0..string.size - 5).map do |start_index|
-#     next [] if non_vowels.include?(string[start_index])
 #     (5..string.size - start_index).map do |length|
 #       string[start_index, length]
 #     end
@@ -11,47 +21,59 @@
   
 #   vowel_only_substrings = all_substrings.select do |substring|
 #     substring.chars.all? do |char|
-#         vowels.include?(char)
+#         VOWELS.include?(char)
 #     end
 #   end
 
 #   vowel_substrings = vowel_only_substrings.select do |substring|
-#     vowels.all? do |vowel|
+#     VOWELS.all? do |vowel|
 #         substring.include?(vowel)
 #     end
 #   end.size
 # end
 
-def count_vowel_substrings(string)
-  vowels = %w(a e i o u)
-  non_vowels = ('a'..'z').to_a - vowels
+# VOWELS = %w(a e i o u)
 
-  all_substrings = (0..string.size - 5).map do |start_index|
-    next [] if non_vowels.include?(string[start_index])
-    (5..string.size - start_index).map do |length|
-      string[start_index, length]
-    end
-  end.flatten
-  
-  vowel_only_substrings = all_substrings.select do |substring|
-    substring.chars.all? do |char|
-        vowels.include?(char)
-    end
-  end
+# def count_vowel_substrings(string)
+#   all_substrings = get_all_substrings(string)
+#   vowel_substrings = filter_substrings(all_substrings)
+#   vowel_substrings.size
+# end
 
-  vowel_substrings = vowel_only_substrings.select do |substring|
-    vowels.all? do |vowel|
-        substring.include?(vowel)
-    end
-  end.size
-end
+# def get_all_substrings(string)
+#   (0..string.size - 5).map do |start_index|
+#     get_all_lengths(string, start_index)
+#   end.flatten
+# end
 
+# def get_all_lengths(string, start_index)
+#   (5..string.size - start_index).map do |length|
+#     string[start_index, length]
+#   end
+# end
 
+# def filter_substrings(all_substrings)
+#   vowel_only_substrings = get_vowel_only_substrings(all_substrings)
+#   vowel_substrings = get_vowel_substrings(vowel_only_substrings)
+# end
 
+# def get_vowel_only_substrings(array)
+#   array.select do |substring|
+#     substring.chars.all? do |char|
+#         VOWELS.include?(char)
+#     end
+#   end
+# end
 
+# def get_vowel_substrings(array)
+#   array.select do |substring|
+#     VOWELS.all? do |vowel|
+#         substring.include?(vowel)
+#     end
+#   end
+# end
 
-
-p count_vowel_substrings("abcde") == 0
-p count_vowel_substrings("cuaieuouac") == 7
-p count_vowel_substrings("aeiouu") == 2
-p count_vowel_substrings("unicornarihan") == 0
+# p count_vowel_substrings("abcde") == 0
+# p count_vowel_substrings("cuaieuouac") == 7
+# p count_vowel_substrings("aeiouu") == 2
+# p count_vowel_substrings("unicornarihan") == 0
