1bc0dad126edb7c2544c2c76334498ebc82e622b
Mitch Mills <mitchmillsgcp@gmail.com>
2023-09-15 17:27:02 -0500

    PEDAC practice

--- 00Practice/PEDAC_Practice/worked_problems.rb
+++ 00Practice/PEDAC_Practice/worked_problems.rb
@@ -1,8 +1,34 @@
+def next_bigger_num(number)
+  digits = number.digits
+  combos = digits.permutation(digits.size).uniq.map(&:join).map(&:to_i).sort
+  index = combos.index(number)
+  combos[index + 1] || -1
+end
+
+
+p next_bigger_num(9) == -1
+p next_bigger_num(12) == 21
+p next_bigger_num(513) == 531
+p next_bigger_num(2017) == 2071
+p next_bigger_num(111) == -1
+p next_bigger_num(531) == -1
+p next_bigger_num(123456789) == 123456798
+
 ### DELETE A DIGIT
 # def delete_digit(number)
 #   digits = number.digits.reverse
 
 #   possibles = (0...digits.size).map do |index|
+#     digits.select.with_index { |_, idx| idx != index }
+#   end
+
+#   possibles.map(&:join).map(&:to_i).sort.last
+# end
+
+# def delete_digit(number)
+#   digits = number.digits.reverse
+
+#   possibles = digits.each_index.map do |index|
 #     digits.select.with_index { |_, idx| idx != index }
 #   end
 
@@ -20,7 +46,7 @@
 #   possibles.map(&:to_i).sort.last
 # end
 
-# p delete_digit(791983) #== 91983
+# p delete_digit(791983) == 91983
 # p delete_digit(152) == 52
 # p delete_digit(1001) == 101
 # p delete_digit(10) == 1
