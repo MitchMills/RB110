0002608c3e5d15d1298c13f6b636e4edf0e925a6
Mitch Mills <mitchmillsgcp@gmail.com>
2023-10-05 08:08:59 -0500

    Worked on medium 1 exercises

--- 00Practice/Small_Problems_Exercises/medium1.rb
+++ 00Practice/Small_Problems_Exercises/medium1.rb
@@ -1,9 +1,39 @@
 ### 6 STACK MACHINE INTERPRETATION
 def minilang(instructions)
-
+  register = 0
+  stack = []
+  instructions.split.each do |instruction|
+    case instruction
+    when 'PUSH'   then stack << register
+    when 'ADD'    then register += stack.pop
+    when 'SUB'    then register -= stack.pop
+    when 'MULT'   then register *= stack.pop
+    when 'DIV'    then register /= stack.pop
+    when 'MOD'    then register %= stack.pop
+    when 'POP'    then register = stack.pop
+    when 'PRINT'  then puts register
+    else register = instruction.to_i
+    end
+  end
+  p register
+  p stack
 end
 
+(3 + (4 * 5) - 7) / (5 % 3)
+(3 + (20) - 7) / 2
+16 / 4
 
+ 
+# minilang('PRINT') # 0
+# minilang('5 PUSH 3 MULT PRINT') # 15
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT') # 5 # 3 # 8
+# minilang('5 PUSH POP PRINT') # 5
+# minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# # 5 # 10 # 4 # 7
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ') # 6
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ') # 12
+# minilang('-3 PUSH 5 SUB PRINT') # 8
+minilang('6 PUSH')
 
 ### 5 DIAMONDS!
 ### initial solution
