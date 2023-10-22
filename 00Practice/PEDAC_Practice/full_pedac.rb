5b90b4266f63f61419bed1618b53684286d315b9
Mitch Mills <mitchmillsgcp@gmail.com>
2023-10-22 09:07:28 -0500

    PEDAC practice problems

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,4 +1,78 @@
-###
+
+
+
+### ANAGRAMS
+=begin
+Write a method that takes two strings as arguments and returns true if they are anagrams of each other, false otherwise. For this problem, two strings are considered anagrams if they both contain all the same type and number of letters, regardless of case. All input strings contain only letter characters.
+
+p is_anagram?('Creative', 'Reactive') == true
+p is_anagram?('bold', 'DLOB') == true
+p is_anagram?('apple', 'apple') == true
+p is_anagram?('Apple', 'apple') == true
+p is_anagram?('apple', 'apply') == false
+p is_anagram?('apple', 'appl') == false
+p is_anagram?('', '') == true
+
+7:40
+PROBLEM
+input: two strings
+  - contain only letter characters
+  - letters may be upper or lower case
+  - may be empty
+output: boolean
+  - true if input strings are anagrams
+    - anagram means all the same number and type of letters, regardless of case
+    - two identical strings are considered anagrams
+    - strings do not have to be actual words
+    - two empty strings are considered anagrams of each other
+  - false if not
+
+EXAMPLES
+
+DATA STRUCTURES
+- input: two strings: 'bold', 'DLOB'
+  - arrays of individual characters: ['b', 'o', 'l', 'd'], ['B', 'O', 'L', 'D']
+  - sorted, downcased: ['b', 'd', 'l', 'o'], ['b', 'd', 'l', 'o']
+- output: boolean
+
+ALGORITHM
+- separate each input string into an array of individual characters
+- downcase all letters in array
+- sort arrays alphabetically
+- compare to see if they are the same
+=end
+
+# def is_anagram?(string1, string2)
+#   chars1 = string1.downcase.chars.sort
+#   chars2 = string2.downcase.chars.sort
+#   chars1 == chars2
+# end
+
+# def is_anagram?(string1, string2)
+#   chars1, chars2 = [string1, string2].map { |str| str.downcase.chars.sort }
+#   chars1 == chars2
+# end
+
+# def is_anagram?(string1, string2)
+#   string1.downcase.chars.sort == string2.downcase.chars.sort
+# end
+
+# def is_anagram?(string1, string2)
+#   chars1, chars2 = [string1, string2].map { |str| str.downcase.chars.tally }
+#   chars1 == chars2
+# end
+
+# def is_anagram?(string1, string2)
+#   string1.downcase.chars.tally == string2.downcase.chars.tally
+# end
+
+# p is_anagram?('Creative', 'Reactive') #== true
+# p is_anagram?('bold', 'DLOB') == true
+# p is_anagram?('apple', 'apple') == true
+# p is_anagram?('Apple', 'apple') == true
+# p is_anagram?('apple', 'apply') == false
+# p is_anagram?('apple', 'appl') == false
+# p is_anagram?('', '') == true
 
 
 
@@ -46,18 +120,18 @@ ALGORITHM
 - return 0 if sum of no subarrays (including full input array) exceed target sum
 =end
 
-def shortest_length(numbers, target_sum)
-  (1..numbers.size).each do |current_size|
-    (0...numbers.size).each do |start_index|
-      return current_size if numbers[start_index, current_size].sum >= target_sum
-    end
-  end
-  0
-end
+# def shortest_length(numbers, target_sum)
+#   (1..numbers.size).each do |current_size|
+#     (0...numbers.size).each do |start_index|
+#       return current_size if numbers[start_index, current_size].sum >= target_sum
+#     end
+#   end
+#   0
+# end
 
-p shortest_length([2, 3, 1, 2, 4, 3], 7) == 2
-p shortest_length([1, 10, 5, 2, 7], 9) == 1
-p shortest_length([1, 11, 100, 1, 0, 200, 3, 2, 1, 250], 280) == 4
-p shortest_length([1, 2, 4, 1], 8) == 4
-p shortest_length([1, 2, 4], 8) == 0
-p shortest_length([], 1) == 0
+# p shortest_length([2, 3, 1, 2, 4, 3], 7) == 2
+# p shortest_length([1, 10, 5, 2, 7], 9) == 1
+# p shortest_length([1, 11, 100, 1, 0, 200, 3, 2, 1, 250], 280) == 4
+# p shortest_length([1, 2, 4, 1], 8) == 4
+# p shortest_length([1, 2, 4], 8) == 0
+# p shortest_length([], 1) == 0
