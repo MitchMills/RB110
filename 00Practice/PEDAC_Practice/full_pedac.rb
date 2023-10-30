7b64c91b100fb202f707ae052068a21be46168b2
Mitch Mills <mitchmillsgcp@gmail.com>
2023-10-30 08:46:27 -0500

    PEDAC practice problems

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -3,6 +3,58 @@
 
 
 
+### MULTIPLY ALL PAIRS
+=begin
+Write a method that, given two arrays each containing lists of numbers, returns a new array that contains the product of every pair of numbers that can be formed between the elements of the two arrays. The results should be sorted by increasing value.
+
+You may assume that neither argument is an empty array.
+
+Examples:
+p multiply_all_pairs([3], [1, 3, 2]) == [3, 6, 9]
+p multiply_all_pairs([1, 2], [3, 4]) == [3, 4, 6, 8]
+p multiply_all_pairs([1, 2], [4, 3, 1, 2]) == [1, 2, 2, 3, 4, 4, 6, 8]
+
+8:38
+PROBLEM / EXAMPLES
+inputs: 2 arrays
+  - contain integers
+  - not empty
+
+output: array
+  - contains product of every possible pair of elements, one from each input array
+  - should be sorted by increasing value
+  - size is product of the sizes of each input array
+
+DATA STRUCTURES
+input: two arrays
+
+output: array
+
+ALGORITHM
+- iterate over first input array
+  - iterate over second input array
+    - multiply current element 1 * current element 2
+    - store products in an output array
+- sort output array and return it
+=end
+
+# def multiply_all_pairs(array1, array2)
+#   results = []
+#   array1.each do |number1|
+#     array2.each do |number2|
+#       results << (number1 * number2)
+#     end
+#   end
+#   results.sort
+# end
+  
+# p multiply_all_pairs([3], [1, 3, 2]) == [3, 6, 9]
+# p multiply_all_pairs([1, 2], [3, 4]) == [3, 4, 6, 8]
+# p multiply_all_pairs([1, 2], [4, 3, 1, 2]) == [1, 2, 2, 3, 4, 4, 6, 8]
+
+
+
+
 
 ### NEXT BIGGEST NUMBER
 =begin
