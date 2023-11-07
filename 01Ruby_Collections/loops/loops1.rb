d46146cc274ff46168880e0ac518876b4824af2f
Mitch Mills <mitchmillsgcp@gmail.com>
2023-11-07 08:18:02 -0600

    Loops review

--- 01Ruby_Collections/loops/loops1.rb
+++ 01Ruby_Collections/loops/loops1.rb
@@ -1,3 +1,69 @@
+### PRINT UNTIL
+numbers = [7, 9, 13, 25, 18]
+
+### COUNT UP
+# count = 1
+
+# until count > 10
+#   puts count
+#   count += 1
+# end
+
+### PRINT WHILE
+# numbers = []
+
+# while numbers.size < 5
+#   numbers << rand(0..99)
+# end
+
+# puts numbers
+
+### SAY HELLO
+# say_hello = true
+# counter = 1
+
+# while say_hello
+#   puts 'Hello!'
+#   counter += 1
+#   say_hello = false if counter > 5
+# end
+
+### LOOP ON COMMAND
+# loop do
+#   puts 'Should I stop looping?'
+#   answer = gets.chomp
+#   break if answer == 'yes'
+# end
+
+### CONTROL THE LOOP
+# iterations = 1
+
+# loop do
+#   puts "Number of iterations = #{iterations}"
+#   break if iterations >= 5
+#   iterations += 1
+# end
+
+### LOOPCEPTION
+# loop do
+#   puts 'This is the outer loop.'
+
+#   loop do
+#     puts 'This is the inner loop.'
+#     break
+#   end
+#   break
+# end
+
+# puts 'This is outside all loops.'
+
+### 1 RUNAWAY LOOP
+# loop do
+#   puts 'Just keep printing...'
+#   break
+# end
+
+
 ### 10 GREET YOUR FRIENDS
 # friends = ['Sarah', 'John', 'Hannah', 'Dave']
 
