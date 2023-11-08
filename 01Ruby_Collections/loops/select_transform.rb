b1464df9d94ea49ee4463738e4576da7be6200af
Mitch Mills <mitchmillsgcp@gmail.com>
2023-11-08 17:08:58 -0600

    Select and transform review

--- 01Ruby_Collections/loops/select_transform.rb
+++ 01Ruby_Collections/loops/select_transform.rb
@@ -1,3 +1,23 @@
+# def select_letter(string, letter)
+#   result = ''
+#   index = 0
+
+#   loop do
+#     break if index >= string.size
+#     current_letter = string[index]
+#     result << current_letter if current_letter == letter
+#     index += 1
+#   end
+
+#   result
+# end
+
+
+# question = 'How many times does a particular character appear in this sentence?'
+# p select_letter(question, 'a') == "aaaaaaaa"
+# p select_letter(question, 't') == "ttttt"
+# p select_letter(question, 'z') == ""
+
 ##### multiply, non-mutating
 # def multiply(numbers, multiplier)
 #   result = []
