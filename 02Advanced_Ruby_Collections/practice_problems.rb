2f9acfabdddb30228576e3db086780b980e95afc
Mitch Mills <mitchmillsgcp@gmail.com>
2023-11-23 16:27:49 -0600

    Practice problems review

--- 02Advanced_Ruby_Collections/practice_problems.rb
+++ 02Advanced_Ruby_Collections/practice_problems.rb
@@ -1,21 +1,28 @@
 ##### 55555
-def total_age(family, gender)
-  total = 0
-  family.each_value do |info|
-    total += info['age'] if info['gender'] == gender
-  end
-  total
-end
+# def total_age(family, gender)
+#   total = 0
+#   family.each_value do |info|
+#     total += info['age'] if info['gender'] == gender
+#   end
+#   total
+# end
 
-munsters = {
-  "Herman" => { "age" => 32, "gender" => "male" },
-  "Lily" => { "age" => 30, "gender" => "female" },
-  "Grandpa" => { "age" => 402, "gender" => "male" },
-  "Eddie" => { "age" => 10, "gender" => "male" },
-  "Marilyn" => { "age" => 23, "gender" => "female"}
-}
+# def total_age(family, gender)
+#   family.each_value.inject(0) do |total, info|
+#     addition = info['gender'] == gender ? info['age'] : 0
+#     total + addition
+#   end
+# end
 
-p total_age(munsters, 'male') == 444
+# munsters = {
+#   "Herman" => { "age" => 32, "gender" => "male" },
+#   "Lily" => { "age" => 30, "gender" => "female" },
+#   "Grandpa" => { "age" => 402, "gender" => "male" },
+#   "Eddie" => { "age" => 10, "gender" => "male" },
+#   "Marilyn" => { "age" => 23, "gender" => "female"}
+# }
+
+# p total_age(munsters, 'male') #== 444
 
 ##### 44444
 # arr1 = [1, [2, 3], 4]
