e34989ce3ffcae80e745b2e7e6475b2182fc2603
Mitch Mills <mitchmillsgcp@gmail.com>
2023-12-12 08:06:52 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,3 +1,117 @@
+### TOP THREE WORDS
+=begin
+Top Three Words
+Write a function that, given a string of text (possibly with punctuation and line-breaks), returns an array of the top-3 most occurring words, in descending order of the number of occurrences.
+
+Assumptions:
+A word is a string of letters (A to Z) optionally containing one or more apostrophes (') in ASCII. (No need to handle fancy punctuation.)
+Matches should be case-insensitive, and the words in the result should be lowercased.
+Ties may be broken arbitrarily.
+If a text contains fewer than three unique words, then either the top-2 or top-1 words should be returned, or an empty array if a text contains no words.
+
+Examples:
+top_3_words("In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.")
+# => ["a", "of", "on"]
+
+top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")
+# => ["e", "ddd", "aa"]
+
+top_3_words(" //wont won't won't")
+# => ["won't", "wont"]
+
+Bonus points (not really, but just for fun):
+Avoid creating an array whose memory footprint is roughly as big as the input text.
+Avoid sorting the entire array of unique words.
+
+7:13am
+PROBLEM / EXAMPLES
+input: string
+  - can contain punctuation and line breaks
+
+output: array
+  - three elements
+    - top three most common words in input string
+      - descending order (most common, 2nd most common, 3rd most common)
+      - all lowercase letters
+    - word:
+      - string of alphabetic characters
+        - may contain one or more apostrophes
+        - matches should be case-insensitive ('top' and 'Top' are treated as same word)
+    - ties can be broken arbitrarily
+    - if fewer than three unique words:
+      - return top two, or top one
+      - return empty array if no words
+
+DATA STRUCTURES
+- input: string
+  - array of individual words in string
+  - downcased array of words
+  - hash of words and occurences: keys are words, values are counts
+  - sorted hash
+  - top three keys
+- output: array
+
+ALGORITHM
+- separate input string into an array of individual words
+  - eliminate all words that don't contain at least one alphabetic character
+    - downcase all words
+    - separate each word into an array of individual characters
+    - compare against an array of lowercase alphabetic characters
+- get count of occurences of each word
+  - sort from most common to least common
+- get three most common words
+- return in an array
+=end
+
+VALID_CHARACTERS = ('a'..'z').to_a + ["'"]
+
+result = ["", "e", ""].select do |word|
+  word.chars.all? { |char| VALID_CHARACTERS.include?(char) }
+end
+p VALID_CHARACTERS.include?("")
+
+def top_3_words(text)
+  words = get_words(text)
+  # counts = get_word_counts(words)
+  # counts.keys.take(3)
+end
+
+def get_words(text)
+  all_words = text.split.map(&:downcase)
+  clean_words = all_words.map do |word|
+    word.chars.select { |char| VALID_CHARACTERS.include?(char) }.join
+  end
+  p clean_words
+  clean_words.select do |word|
+    word.chars.all? { |char| VALID_CHARACTERS.include?(char) }
+  end
+end
+
+def get_word_counts(words)
+  words.tally.sort_by { |word, count| [count, word] }.to_h
+end
+
+# p top_3_words("a a a b c c d d d d e e e e e") == ["e", "d", "a"]
+# p top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e") #== ["e", "ddd", "aa"]
+# p top_3_words(" //wont won't won't ") == ["won't", "wont"]
+# p top_3_words(" , e .. ") #== ["e"]
+# p top_3_words(" ... ") #== []
+# p top_3_words(" ' ") == []
+# p top_3_words(" ''' ") == []
+# p top_3_words("""In a village of La Mancha, the name of which I have no desire to call to
+# mind, there lived not long since one of those gentlemen that keep a lance
+# in the lance-rack, an old buckler, a lean hack, and a greyhound for
+# coursing. An olla of rather more beef than mutton, a salad on most
+# nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+# on Sundays, made away with three-quarters of his income.""") == ["a", "of", "on"]
+
+
+
 
 
 
