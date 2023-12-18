8164928693194ffc795ee429b0b0d33975a53eb1
Mitch Mills <mitchmillsgcp@gmail.com>
2023-12-18 11:03:43 -0600

    Worked on board display

--- 03Slightly_Larger_Programs/TicTacToe/tictactoe.rb
+++ 03Slightly_Larger_Programs/TicTacToe/tictactoe.rb
@@ -4,62 +4,18 @@ end
 
 def display_board(board)
   puts
-  puts "     |     |     "
+  puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}  "
-  puts "     |     |     "
+  puts "     |     |"
   puts "-----+-----+-----"
-  puts "     |     |     "
+  puts "     |     |"
   puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}  "
-  puts "     |     |     "
+  puts "     |     |"
   puts "-----+-----+-----"
-  puts "     |     |     "
+  puts "     |     |"
   puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}  "
-  puts "     |     |     "
+  puts "     |     |"
 end
 
 board = initialize_board
 display_board(board)
-
-
-     |     |
-     |     |
-     |     |
------+-----+-----
-     |     |
-     |     |
-     |     |
------+-----+-----
-     |     |
-     |     |
-     |     |
-
-
-
-pattern = [space, line, space, line, space]
-
-empty_row_multiples = [5, 1]
-space, line = [' ', '|']
-empty_row =   "     |     |     "
-
-space = [space, mark, space]
-marked_row_multiples = [2, 1]
-space, line = [' ', '|']
-marked_row =  "  X  |  X  |  X  "
-
-line_multiples = [5, 1]
-space, line = ['-', '+']
-line =        "-----+-----+-----"
-
-column_pattern = [empty_row, marked_row, empty_row]
-
-big_row = [column, line, column, line, column]
-
-board_pattern = [big_row, line, big_row, line, big_row]
-
-
-
-  
-
-
-
-p board_dimensions
