dd76fca91aa03d4b2e559a62c15883c29a40f332
Mitch Mills <mitchmillsgcp@gmail.com>
2023-12-18 07:40:52 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,6 +1,77 @@
 
 
 
+### DUBSTEP
+=begin
+DUBSTEP
+Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
+
+Let's assume that a song consists of some number of words (that don't contain WUB). To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
+
+For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and
+cannot transform into "WUBWUBIAMWUBX".
+
+Recently, Jonny has heard Polycarpus's new dubstep track, but since he isn't into modern music, he decided to find out what was the initial song that Polycarpus remixed. Help Jonny restore the original song.
+
+Input
+The input consists of a single non-empty string, consisting only of uppercase English letters, the string's length doesn't exceed 200 characters
+
+Output
+Return the words of the initial song that Polycarpus used to make a dubsteb remix. Separate the words with a space.
+
+Examples
+song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB") => WE ARE THE CHAMPIONS MY FRIEND
+
+p song_decoder("A WUB B WUB C") == "A B C"
+p song_decoder("A WUBWUBWUB B WUBWUBWUB C") == "A B C"
+p song_decoder("WUB A WUB B WUB C WUB") == "A B C"
+
+7:00
+PROBLEM
+input: string
+  - not empty
+  - only uppercase alphabetic characters
+  - max length is 200 characters
+
+output: string
+  - represents a song title
+  - words separated with spaces
+  - all uppercase alphabetic characters
+  - interstitial WUBs removed
+  - song title does not contain any WUBs
+  - at least one WUB between each word
+  - can be one or more WUBs at beginning and end of string
+
+EXAMPLES
+
+DATA STRUCTURES
+- input: string
+  - array of all possible three-character substrings
+    - "AWUBBWUBC" [AWU, WUB, UBB, BBW, BWU, WUB, UBC]
+    - indexes of beginning of each wub [1, 5]: use to remove three-character slices from input string
+- output: string
+
+ALGORITHM
+- get all possible three-character substrings from input string
+  - start at index 0, up to index (input string length - 3)
+  - length is alway 3
+- get indexes of each WUB in that array
+  - indexes will be start indexes of slices
+- use indexes to remove three-character slices from input string
+- return string, separated by spaces
+=end
+
+# def song_decoder(string)
+#   letters = string.split('WUB')
+#   letters.select { |letter| letter != '' }.join(' ')
+# end
+
+# p song_decoder("AWUBBWUBC") == "A B C"
+# p song_decoder("AWUBWUBWUBBWUBWUBWUBC") == "A B C"
+# p song_decoder("WUBAWUBBWUBCWUB") == "A B C"
+# p song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB") == 'WE ARE THE CHAMPIONS MY FRIEND'
+
+
 
 
 ### KEBABIZE
