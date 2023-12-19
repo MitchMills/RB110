df34aba9ced4fe5cfc43f67107bae8ec1663e02c
Mitch Mills <mitchmillsgcp@gmail.com>
2023-12-19 07:21:40 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,6 +1,63 @@
 
 
 
+
+### TEN MINUTE WALK
+=begin
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block in a direction and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point.
+
+Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+p is_valid_walk(['n','s','n','s','n','s','n','s','n','s']) == true
+p is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']) == false
+p is_valid_walk(['w']) == false
+p is_valid_walk(['n','n','n','s','n','s','n','s','n','s']) == false
+
+7:10
+PROBLEM
+input: array
+  - contains one or more one character strings
+  - each string contains one of four letters: n s e w
+  - represents a 'walk'
+    - each letter counts as a one minute walk in that direction
+    
+output: boolean
+  - true if:
+    - there are exactly ten elements in the input array
+    - there are a balanced set of n,s and e,w pairs
+      - i.e. number of n = number of s, nubmer of e = number of w
+  - false otherwise
+
+DATA STRUCTURES
+- input: array
+  - count of elements in input array
+  - count of each letter in input array:
+    - hash: keys are letters, values are counts
+- output: boolean
+
+ALGORITHM
+- count the number of elements in the input array
+  - return false unless size == 10
+- count the number of occurences of each letter in input array
+  - return false unless n == s && e == w
+=end
+
+# def valid_walk?(directions)
+#   counts = directions.tally
+#   counts.values.sum == 10 &&
+#   counts['n'] == counts['s'] && 
+#   counts['e'] == counts['w']
+# end
+
+# p valid_walk?(['n','s','n','s','n','s','n','s','n','s']) == true
+# p valid_walk?(['w','e','w','e','w','e','w','e','w','e','w','e']) == false
+# p valid_walk?(['w']) == false
+# p valid_walk?(['n','n','n','s','n','s','n','s','n','s']) == false
+
+
+
 ### DUBSTEP
 =begin
 DUBSTEP
