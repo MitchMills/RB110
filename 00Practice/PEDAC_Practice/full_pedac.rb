0f120cd7df66392b864dad19fff8dde5eda5bf35
Mitch Mills <mitchmillsgcp@gmail.com>
2023-12-23 07:50:37 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -51,9 +51,17 @@ ALGORITHM
 #   expanded.select { |element| element > 0 }.join(' + ')
 # end
 
-# p expanded_form(12) == '10 + 2'
-# p expanded_form(42) == '40 + 2'
-# p expanded_form(70304) == '70000 + 300 + 4'
+# def expanded_form(integer)
+#   digits = integer.digits
+#   expanded = digits.map.with_index do |digit, index|
+#     digit * 10**index
+#   end.reverse
+#   expanded.select { |element| element > 0 }.join(' + ')
+# end
+
+# p expanded_form(12) #== '10 + 2'
+# p expanded_form(42) #== '40 + 2'
+# p expanded_form(70304) #== '70000 + 300 + 4'
 
 
 
