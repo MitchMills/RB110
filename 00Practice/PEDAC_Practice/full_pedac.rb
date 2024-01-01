aeb058800617918fef4d6a88dee949022fe4e289
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-01 08:37:22 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,3 +1,47 @@
+### MEAN SQUARE ERROR
+=begin
+Complete the function that accepts two integer arrays of equal length compares the value each member in one array to the corresponding member in the other squares the absolute value difference between those two values and returns the average of those squared absolute value difference between each member pair.
+Examples
+[1, 2, 3], [4, 5, 6] --> 9 because (9 + 9 + 9) / 3
+[10, 20, 10, 2], [10, 25, 5, -2] --> 16.5 because (0 + 25 + 25 + 16) / 4
+[-1, 0], [0, -1] --> 1 because (1 + 1) / 2
+
+8:25
+PROBLEM
+input: two arrays
+  - equal length to each other
+  - contain only integers
+
+output: float
+  - each integer in first array is compared to corresponding integer in second array
+    - get absolute value difference between them
+    - square that number
+  - squares are added up and divided by length of arrays (float division)
+
+EXAMPLES
+
+DATA STRUCTURES
+- input: two arrays
+  - array of squares
+- output: float
+
+ALGORITHM
+- get differences between each pair of corresponding numbers
+- square those numbers
+- divide by length of arrays
+- return that number
+=end
+
+# def solution(array1, array2)
+#   squares = (0...array1.size).map do |index|
+#     (array1[index] - array2[index]).abs**2
+#   end
+#   (squares.sum).to_f / array1.size
+# end
+
+# p solution([1, 2, 3], [4, 5, 6]) == 9
+# p solution([10, 20, 10, 2], [10, 25, 5, -2]) == 16.5
+# p solution([-1, 0], [0, -1]) == 1
 
 
 
