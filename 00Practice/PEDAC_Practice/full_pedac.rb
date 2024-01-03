868760a1f044f31a3f53705be7d7f5bf9504da11
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-03 16:25:36 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,5 +1,51 @@
 
 
+
+
+### ANAGRAMS LIST
+=begin
+ANAGRAMS LIST
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+'abba' & 'baab' == true
+'abba' & 'bbaa' == true
+'abba' & 'abbba' == false
+'abba' & 'abca' == false
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none.
+
+4:12
+PROBLEM
+input: string, array
+  - string is a single word
+  - array is a list of words
+output: array
+  - list of words from input array that are anagrams of input string
+    - anagrams: both words contain all and only the same letters in same amounts (can be the same word)
+    - case sensitive? decision: yes
+  - return empty array if input array contains no anagrams of input string
+
+EXAMPLES
+
+DATA STRUCTURES
+- input: string, array
+  - string and array words split into individual characters and sorted
+- output: array
+
+ALGORITHM
+- iterate over input array
+  - select elements that are anagrams of input string
+    - separate into individual characters and then sort in order to compare
+=end
+
+# def anagrams(target, words)
+#   words.select { |word| word.chars.sort == target.chars.sort }
+# end
+
+# p anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) == ['aabb', 'bbaa']
+# p anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) == ['carer', 'racer']
+# p anagrams('laser', ['lazing', 'lazy', 'lacer']) == []
+
+
+
 ### EXPONENTS
 =begin
 EXPONENTS
