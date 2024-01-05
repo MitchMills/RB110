b100d2c0f2abafcd59b80991a1cb77f5ca7943c0
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-05 10:07:40 -0600

    Worked on main game loop

--- 03Slightly_Larger_Programs/TicTacToe/2tictactoe.rb
+++ 03Slightly_Larger_Programs/TicTacToe/2tictactoe.rb
@@ -2,7 +2,15 @@ PLAYER_MARK = 'X'
 COMPUTER_MARK = 'O'
 EMPTY_MARK = ' '
 
-ALL_SQUARES = ('1'..'9').to_a
+ALL_SQUARES = (1..9).to_a
+CORNER_SQUARES = [1, 3, 7, 9]
+CENTER_SQUARE = [5]
+
+WINNING_LINES = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9],  # horizontal
+  [1, 4, 7], [2, 5, 8], [3, 6, 9],  # vertical
+  [1, 5, 9], [3, 5, 7]              # diagonal
+]
 
 # game setup methods
 def new_board
@@ -54,7 +62,4 @@ end
 
 board = {"1"=>"X", "2"=>"O", "3"=>" ", "4"=>" ", "5"=>" ", "6"=>" ", "7"=>" ", "8"=>"X", "9"=>" "}
 display_board(board)
-p empty_squares(board)
 
-
-# display_board(board)
