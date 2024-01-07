e64564b1b1c589939685b4624f653837f2f431df
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-07 09:19:57 -0600

    Worked on board display

--- 03Slightly_Larger_Programs/TicTacToe/board.rb
+++ 03Slightly_Larger_Programs/TicTacToe/board.rb
@@ -14,33 +14,35 @@ WINNING_LINES = [
 
 LINE_TYPES = {
   empty:    {pattern: [:fill, :mark, :fill], fill: (' ' * 2), mark: ' ', limit: '|'},
-  marked:   {pattern: [:fill, :mark, :fill], fill: (' ' * 2), mark: 'X', limit: '|'},
+  marked:   {pattern: [:fill, :mark, :fill], fill: (' ' * 2), mark: '?', limit: '|'},
   numbered: {pattern: [:fill, :fill, :mark], fill: (' ' * 2), mark: '#', limit: '|'},
   lined:    {pattern: [:fill, :mark, :fill], fill: ('-' * 2), mark: '-', limit: '+'}
 }
 
-def sub_line(line_type, square_num)
+def sub_line(line_type, square_num, board)
   elements = LINE_TYPES[line_type]
+  elements[:mark] = board[square_num] if line_type == :marked
+  elements[:mark] = square_num.to_s if line_type == :numbered
   sub_line = elements[:pattern].map { |type| elements[type] }.join
   square_num % 3 == 0 ? sub_line : sub_line << elements[:limit]
 end
 
-def full_line(line_type, squares)
-  squares.map { |square_number| sub_line(line_type, square_number) }.join
+def full_line(line_type, squares, board)
+  squares.map { |square_number| sub_line(line_type, square_number, board) }.join
 end
 
-def row(row_number)
+def row(row_number, board)
   squares = case row_number
-            when 0 then [1, 2, 3]
-            when 1 then [4, 5, 6]
-            when 2 then [7, 8, 9]
+            when 1 then [1, 2, 3]
+            when 2 then [4, 5, 6]
+            when 3 then [7, 8, 9]
             end
-  row = [:empty, :marked, :numbered].map { |line_type| full_line(line_type, squares) }
-  row_number == 2 ? row : row << full_line(:lined, squares)
+  row = [:empty, :marked, :numbered].map { |line_type| full_line(line_type, squares, board) }
+  row_number < 3 ? row << full_line(:lined, squares, board) : row
 end
 
 def display_board(board)
-  (0..2).each { |row_number| puts row(row_number) } # could be (1..3)???
+  (1..3).each { |row_number| puts row(row_number, board) } # could be (1..3)???
 end
 
 # game setup methods
@@ -51,7 +53,7 @@ end
 # board = new_board
 # p new_board
 
-board = {1=>" ", 2=>" ", 3=>" ", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>" ", 9=>" "}
+board = {1=>"X", 2=>" ", 3=>" ", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>" ", 9=>" "}
 display_board(board)
 #TODO: get board information into display methods (mark, square number)
 
