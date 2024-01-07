dd5bd9fc53d73529c7b2810134f30e4218205f4d
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-07 12:32:21 -0600

    Worked on board display

--- 03Slightly_Larger_Programs/TicTacToe/board.rb
+++ 03Slightly_Larger_Programs/TicTacToe/board.rb
@@ -12,22 +12,32 @@ WINNING_LINES = [
   [1, 5, 9], [3, 5, 7]              # diagonal
 ]
 
-LINES_DATA = {
+BOARD_PARTS = {
+  row:      {pattern: [:empty, :marked, :numbered]},
   empty:    {pattern: [:fill, :mark, :fill], fill: (' ' * 2), mark: ' ', limit: '|'},
   marked:   {pattern: [:fill, :mark, :fill], fill: (' ' * 2), mark: '?', limit: '|'},
   numbered: {pattern: [:fill, :fill, :mark], fill: (' ' * 2), mark: ' ', limit: '|'},
   lined:    {pattern: [:fill, :mark, :fill], fill: ('-' * 2), mark: '-', limit: '+'}
 }
 
-# board display methods
-def display_board(board)
-  puts
-  (0..2).each { |row_number| puts row(row_number, board) }
+# general methods
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def blank_line
   puts
 end
 
+# board display methods
+def display_board(board)
+  blank_line
+  (0..2).each { |row_number| puts row(row_number, board) }
+  blank_line
+end
+
 def row(row_number, board)
-  pattern  = [:empty, :marked, :numbered]
+  pattern  = BOARD_PARTS[:row][:pattern]
   row = pattern.map { |line_type| full_line(line_type, row_number, board) }
   row_number < 2 ? row << full_line(:lined, row_number, board) : row
 end
@@ -39,15 +49,13 @@ def full_line(line_type, row_number, board)
 end
 
 def sub_line(line_type, square_num, board)
-  elements = LINES_DATA[line_type]
-  sub_line = elements[:pattern].map { |type| elements[type] }.join
+  parts = BOARD_PARTS[line_type]
+  sub_line = parts[:pattern].map { |type| parts[type] }.join
 
   sub_line[2] = board[square_num] if line_type == :marked
-
-  sub_line[4] = square_num.to_s if (line_type == :numbered) && 
+  sub_line[3] = square_num.to_s if (line_type == :numbered) && 
     (empty_squares(board).include?(square_num))
-
-  square_num % 3 == 0 ? sub_line : sub_line << elements[:limit]
+  square_num % 3 == 0 ? sub_line : sub_line << parts[:limit]
 end
 
 # game setup methods
@@ -63,7 +71,7 @@ end
 
 
 
-board = {1=>"X", 2=>"O", 3=>" ", 4=>" ", 5=>"O", 6=>"O", 7=>"X", 8=>" ", 9=>" "}
+board = {1=>"O", 2=>" ", 3=>" ", 4=>" ", 5=>"X", 6=>" ", 7=>" ", 8=>" ", 9=>"X"}
 display_board(board)
 # p row(1, board)
 # p full_line(:numbered, 1, board)
