6d0dc8a987eb8574572b8094f31c5ec80d9c8261
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-10 18:00:32 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,3 +1,39 @@
+### MEXICAN WAVE
+=begin
+MEXICAN WAVE
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+Rules
+1. The input string will always be lower case but may be empty.
+2. If the character in the string is whitespace then pass over it as if it was an empty seat.
+
+5:50
+PROBLEM
+input: string
+  - contains only lowercase alpha characters and spaces
+  - may be empty
+
+output: array
+  - if input string is empty, return empty array
+  - array has same number of elements (strings) as letters in input string (skip spaces)
+  - each string in array is the same word(s) as the input string
+    - one letter is capitalized
+    - first letter of first element, second of second, etc
+    - preserve spaces in input string but skip over them
+
+EXAMPLES
+
+DATA STRUCTURES
+- input: string
+  - array of each word in input string # ['two', 'words']
+  -
+- output: array
+=end
+
+p wave("hello") == ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+p wave("codewars") == ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"]
+p wave("") == []
+p wave("two words") == ["Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS"]
+p wave(" gap ") == [" Gap ", " gAp ", " gaP "]
 
 
 
