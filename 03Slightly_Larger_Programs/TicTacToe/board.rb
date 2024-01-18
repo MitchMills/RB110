1d0a624569b16135f8a919ab98c35bd623e0dacb
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-18 07:57:10 -0600

    Refactored board display methods

--- 03Slightly_Larger_Programs/TicTacToe/board.rb
+++ 03Slightly_Larger_Programs/TicTacToe/board.rb
@@ -38,56 +38,56 @@ end
 
 
 # board display methods ###
-def display_board(game_status)
+def display_board(game_data)
   system('clear')
   blank_line
   row_positions = BOARD_PARTS[:rows][:positions].keys
-  row_positions.each { |row_position| puts row(row_position, game_status) }
+  row_positions.each { |row_position| puts row(row_position, game_data) }
   blank_line
 end
 
-def row(row_position, game_status)
+def row(row_position, game_data)
   pattern  = BOARD_PARTS[:rows][:pattern]
   pattern.map do |line_type|
     next if row_position == :bottom && line_type == :horizontal
-    full_line(line_type, row_position, game_status)
+    full_line(line_type, row_position, game_data)
   end
 end
 
-def full_line(line_type, row_position, game_status)
+def full_line(line_type, row_position, game_data)
   row_squares = BOARD_PARTS[:rows][:positions][row_position]
   row_squares.map do |square_number|
-    sub_line(line_type, square_number, game_status)
+    sub_line(line_type, square_number, game_data)
   end.join
 end
 
-def sub_line(line_type, square_number, game_status)
+def sub_line(line_type, square_number, game_data)
   data = BOARD_PARTS[:sub_lines]
   pattern = data[:pattern]
-  parts = line_type == :horizontal ? data[:horizontal] : data[:inner]
+  parts = (line_type == :horizontal) ? data[:horizontal] : data[:inner]
 
   sub_line = pattern.map do |part_type|
-    next if part_type == :limit && (square_number % 3 == 0)
+    next if (part_type == :limit) && (square_number % 3 == 0)
     parts[part_type]
   end.join
 
-  add_info(sub_line, square_number, line_type, game_status)
+  add_info(sub_line, square_number, line_type, game_data)
 end
 
-def add_info(sub_line, square_number, line_type, game_status)
+def add_info(sub_line, square_number, line_type, game_data)
   center_point = BOARD_SQUARE_SIZE / 2
   case line_type
   when :marked
-    sub_line[center_point] = game_status[:board][square_number]
+    sub_line[center_point] = game_data[:board][square_number]
   when :numbered
     sub_line[center_point] = square_number.to_s if
-      empty_squares(game_status).include?(square_number)
+      empty_squares(game_data).include?(square_number)
   end
   sub_line
 end
 
-def empty_squares(game_status)
-  board = game_status[:board]
+def empty_squares(game_data)
+  board = game_data[:board]
   board.keys.select { |square_number| board[square_number] ==  EMPTY_MARK }
 end
 
@@ -101,7 +101,7 @@ def new_board
   ALL_SQUARES.map { |square_number| [square_number, EMPTY_MARK] }.to_h
 end
 
-def determine_first_player(game_status)
+def determine_first_player(game_data)
   choice = nil
   loop do
     choice = get_first_player_choice
@@ -109,8 +109,8 @@ def determine_first_player(game_status)
     prompt("I'm sorry, that's not a valid choice")
     blank_line
   end
-  set_player_order(game_status, choice)
-  display_player_order(game_status)
+  set_player_order(game_data, choice)
+  display_player_order(game_data)
 end
 
 def get_first_player_choice
@@ -122,16 +122,16 @@ def get_first_player_choice
   gets.chomp
 end
 
-def set_player_order(game_status, choice)
+def set_player_order(game_data, choice)
   choice = ['1', '2'].sample if choice == '3'
-  players = game_status[:players]
+  players = game_data[:players]
   players[:player1] = (choice == '1') ? :user : :computer
   players[:player2] = (choice == '1') ? :computer : :user
 end
 
-def display_player_order(game_status)
+def display_player_order(game_data)
   players = ["You", "The computer"]
-  players = players.reverse if game_status[:players][:player1] == :computer
+  players = players.reverse if game_data[:players][:player1] == :computer
   order, marks = %w(first second), [PLAYER1_MARK, PLAYER2_MARK]
 
   players.each_with_index do |player, index|
@@ -146,55 +146,55 @@ end
 
 
 # single game methods
-def play_one_game(game_status)
-  game_status[:board] = new_board
+def play_one_game(game_data)
+  game_data[:board] = new_board
   current_player = :player1 # TODO: need to switch between games in match
   loop do
-    display_board(game_status)
-    player_places_mark!(current_player, game_status)
-    break if game_winner?(game_status) || board_full?(game_status)
+    display_board(game_data)
+    player_places_mark!(current_player, game_data)
+    break if game_winner?(game_data) || board_full?(game_data)
     current_player = switch_player(current_player)
   end
-  display_game_result(game_status)
+  display_game_result(game_data)
 end
 
-def player_places_mark!(current_player, game_status) # player = :player1 or :player2
-  choice =  get_choice(current_player, game_status)
-  update_board(current_player, choice, game_status)
+def player_places_mark!(current_player, game_data) # player = :player1 or :player2
+  choice =  get_choice(current_player, game_data)
+  update_board(current_player, choice, game_data)
 end
 
-def get_choice(current_player, game_status)
-  player = game_status[:players][current_player]
-  player == :user ? user_choice(game_status) : computer_choice(game_status)
+def get_choice(current_player, game_data)
+  player = game_data[:players][current_player]
+  player == :user ? user_choice(game_data) : computer_choice(game_data)
 end
 
-def user_choice(game_status) # TODO: loop for entry validation
-  prompt(:print, "Choose an empty square: #{empty_squares(game_status)}: ")
+def user_choice(game_data) # TODO: loop for entry validation
+  prompt(:print, "Choose an empty square: #{empty_squares(game_data)}: ")
   choice = gets.chomp.to_i
 end
 
-def computer_choice(game_status)
-  empty_squares(game_status).sample
+def computer_choice(game_data)
+  empty_squares(game_data).sample
 end
 
 
 # computer choice methods
-def real_computer_choice(game_status)
-  targets = get_targets(game_status)
+def real_computer_choice(game_data) # change name back to `computer_choice` when working
+  targets = get_targets(game_data)
   targets.each_value do |target_squares|
     return target_squares.sample if target_squares.size > 0
   end
 end
 
-def get_targets(game_status)
+def get_targets(game_data)
   target_types = [:opportunities, :threats, :center, :corners, :other]
   target_types.each_with_object(Hash.new([])) do |type, targets|
-    targets[type] = get_target_squares(type, game_status)
+    targets[type] = get_target_squares(type, game_data)
   end
 end
 
-def get_target_squares(type, game_status)
-  players = game_status[:players]
+def get_target_squares(type, game_data)
+  players = game_data[:players]
   computer_mark = (players[:player1] == :user) ? PLAYER2_MARK : PLAYER1_MARK
   player_mark =   (players[:player1] == :user) ? PLAYER1_MARK : PLAYER2_MARK
 
@@ -234,17 +234,17 @@ end
 
 
 
-def update_board(player, choice, game_status)
+def update_board(player, choice, game_data)
   mark = (player == :player1) ? PLAYER1_MARK : PLAYER2_MARK
-  game_status[:board][choice] = mark
+  game_data[:board][choice] = mark
 end
 
-def game_winner?(game_status)
-  !!detect_game_winner(game_status)
+def game_winner?(game_data)
+  !!detect_game_winner(game_data)
 end
 
-def detect_game_winner(game_status)
-  board, players = game_status[:board], game_status[:players]
+def detect_game_winner(game_data)
+  board, players = game_data[:board], game_data[:players]
   WINNING_LINES.each do |line|
     if board.values_at(*line).count(PLAYER1_MARK) == 3 # TODO: helper method here?
       return players[:player1]
@@ -255,17 +255,17 @@ def detect_game_winner(game_status)
   nil
 end
 
-def board_full?(game_status)
-  empty_squares(game_status).empty?
+def board_full?(game_data)
+  empty_squares(game_data).empty?
 end
 
 def switch_player(current_player)
   current_player == :player1 ? :player2 : :player1
 end
 
-def display_game_result(game_status)
-  display_board(game_status)
-  result =  detect_game_winner(game_status)
+def display_game_result(game_data)
+  display_board(game_data)
+  result =  detect_game_winner(game_data)
   case result
   when :user then prompt("You have won!")
   when :computer then prompt("The computer has won!")
@@ -274,9 +274,9 @@ def display_game_result(game_status)
 end
 
 
-game_status = {
+game_data = {
   board: {1=>" ", 2=>" ", 3=>"X", 4=>" ", 5=>"O", 6=>" ", 7=>"X", 8=>" ", 9=>" "},
-  players: {player1: :computer, player2: :user}
+  players: {player1: :user, player2: :computer}
 }
 
-display_board(game_status)
+display_player_order(game_data)
