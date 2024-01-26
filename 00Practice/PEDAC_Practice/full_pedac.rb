e22230c76a90ee2a2ae235dd17109be8663f2e99
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-26 08:07:04 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -1,4 +1,74 @@
-### 49
+
+
+
+
+
+### 49 PLAYING WITH DIGITS
+=begin
+49 PLAYING WITH DIGITS
+Some numbers have funny properties. For example:
+89 --> 8¹ + 9² = 89 * 1
+695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is
+equal to k * n.
+In other words:
+Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+If it is the case we will return k, if not return -1.
+Note: n and p will always be given as strictly positive integers.
+dig_pow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+dig_pow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+dig_pow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+dig_pow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+PROBLEM
+input: two integers
+  - both positive
+  - first integer provides digits
+  - second integer is starting number for powers
+
+output: integer
+ - positive, or -1 if no solution
+ - take sum of each digit of first input integer taken to successive powers
+    - powers start at second input integer
+    - if sum is evenly divisible by first input integer, return factor
+    = else return -1
+
+EXAMPLES
+
+DATA STRUCTURES
+- input: two integers
+  - array of individual digits [6, 9, 5]
+  - range of powers [2, 3, 4]
+  - array of those digits taken to the appropriate power
+  - sum of that array
+  - array of divisor and remainder
+- output: integer
+
+ALGORITHM
+- create an array of individual digits of the first input integer
+- create a range from the second input integer up to (second input integer + array length - 1)
+- transform the array by multiplying each successive integer by each successive power (use indexes)
+- get the sum of that array
+- get the divisor and remainder of dividing by the input integer
+- if the remainder is 0, return the divisor
+- else return -1
+=end
+
+# def dig_pow(number, starting_power)
+#   digits = number.digits.reverse
+#   powers = (starting_power..(starting_power + digits.size - 1)).to_a
+#   sum = digits.map.with_index { |digit, index| digit**powers[index] }.sum
+#   divisor, remainder = sum.divmod(number)
+#   remainder == 0 ? divisor : -1
+# end
+
+# p dig_pow(89, 1) == 1
+# p dig_pow(92, 1) == -1
+# p dig_pow(46288, 3) == 51
+# p dig_pow(695, 2) == 2
+
 
 
 
