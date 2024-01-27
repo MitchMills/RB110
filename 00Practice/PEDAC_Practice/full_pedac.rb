f4646244857873efd9801f81b6fb33626816f735
Mitch Mills <mitchmillsgcp@gmail.com>
2024-01-27 07:18:21 -0600

    PEDAC practice

--- 00Practice/PEDAC_Practice/full_pedac.rb
+++ 00Practice/PEDAC_Practice/full_pedac.rb
@@ -3,6 +3,105 @@
 
 
 
+### 51 REVERSE OR ROTATE
+=begin
+51 REVERSE OR ROTATE
+The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+If sz is <= 0 or if str is empty return ""
+If sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+
+6:43
+PROBLEM
+input: string, integer
+  - string
+    - contains only digits
+    - can be empty
+  - integer
+    - can be negative, positive, or 0
+    - represents 'chunk size'
+output: string
+  - if chunk size is <= 0, return empty string
+  - if chunk size is greater than string length, return empty string
+  - if input string is empty, return empty string
+
+  - else
+    - separate input string into chunks (substrings) of that size
+    - ignore the 'remainder', i.e. if last chunk is smaller than chunk size, ignore it
+    - if sum of cubes of digits in chunk is even, reverse that chunk
+    - otherwise rotate chunk by one digit left
+    - then join chunks together and return the resulting string
+
+EXAMPLES
+
+DATA STRUCTURES
+- input: string, integer
+  - array of substrings of given chunk size
+  - array of transformed substrings (reversed or rotated)
+- output: string
+
+ALGORITHM
+- get all substrings of chunk size
+  - create a range from 0 up to (string size - 1)
+  - iterate over that range using chunk size to get starting indexes
+  - get substrings of chunk from size from each of those starting indexes
+  - repeat until end of string
+  - remove last substring if it's less than chunk size
+- iterate over each substring and transform:
+  - if sum of cubes of digits is even, reverse chunk
+    - iterate over each digit in chunk
+      - convert it to an integer and cube it
+      - get sum
+      - test if it is even
+  - otherwise rotate chunk
+    - get substring from index 1 to last index
+    - add substring from index 0 to the end
+- join substrings into one string and return it
+=end
+
+# def revrot(string, chunk_size)
+#   return '' if chunk_size <= 0
+#   substrings = get_substrings(string, chunk_size)
+#   substrings.map do |substring|
+#     reverse?(substring) ? substring.reverse : rotate(substring)
+#   end.join
+# end
+
+# def get_substrings(string, chunk_size)
+#   last_index = string.size - 1
+#   all_substrings = (0..last_index).step(chunk_size).map do |start_index|
+#     string[start_index, chunk_size]
+#   end
+#   all_substrings.select { |substring| substring.size == chunk_size }
+# end
+
+# def reverse?(substring)
+#   digits = substring.chars.map do |char|
+#     char.to_i ** 3
+#   end.sum.even?
+# end
+
+# def rotate(substring)
+#   substring[1..-1] << substring[0]
+# end
+
+# p revrot("1234", 0) == ""
+# p revrot("", 0) == ""
+# p revrot("1234", 5) == ""
+# p revrot("733049910872815764", 5) == "330479108928157"
+# p revrot("123456987654", 6) == "234561876549"
+# p revrot("123456987653", 6) == "234561356789"
+# p revrot("66443875", 4) == "44668753"
+# p revrot("66443875", 8) == "64438756"
+# p revrot("664438769", 8) == "67834466"
+# p revrot("123456779", 8) == "23456771"
+# p revrot("", 8) == ""
+# p revrot("123456779", 0) == ""
+# p revrot("563000655734469485", 4) == "0365065073456944"
+
+
+
+
 ### 49 PLAYING WITH DIGITS
 =begin
 49 PLAYING WITH DIGITS
