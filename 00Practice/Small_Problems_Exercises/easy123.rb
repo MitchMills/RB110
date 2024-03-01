4ab6e85b65428b388306d95d1a377809c91ad205
Mitch Mills <mitchmillsgcp@gmail.com>
2024-03-01 12:10:00 -0600

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,24 +1,29 @@
 ###
 
 
-12345678901234567890123456789012345678901234567890123456789012345678901234567890
-
 ### 1.6 Right Triangles
-# def display_triangle(size, right_angle)
-#   vertical, horizontal = right_angle.split
+def display_triangle(size, right_angle)
+  triangle = create_triangle(size, right_angle)
+  puts triangle
+end
 
-#   lines = (1..size).to_a
-#   stars = lines.map { |reps| '*' * reps }
-#   spaces = lines.map { |reps| ' ' * (reps - 1) }.reverse
-#   triangle = stars.zip(spaces).map(&:join)
+def create_triangle(size, right_angle)
+  lines = (1..size).to_a
+  stars = lines.map { |reps| '*' * reps }
+  spaces = lines.map { |reps| ' ' * (reps - 1) }.reverse
+  triangle = stars.zip(spaces).map(&:join)
+  rotate_triangle(triangle, right_angle)
+end
 
-#   triangle.reverse! if vertical == 'upper'
-#   triangle.map!(&:reverse) if horizontal == 'right'
-#   puts triangle
-# end
+def rotate_triangle(triangle, right_angle)
+  vertical, horizontal = right_angle.split
+  triangle.reverse! if vertical == 'upper'
+  triangle.map!(&:reverse) if horizontal == 'right'
+  triangle
+end
 
-# # tests
-# display_triangle(5, 'upper left')
-# display_triangle(5, 'upper right')
-# display_triangle(5, 'lower left')
-# display_triangle(5, 'lower right')
+# tests
+display_triangle(5, 'upper left')
+display_triangle(5, 'upper right')
+display_triangle(5, 'lower left')
+display_triangle(5, 'lower right')
