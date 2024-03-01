5b5d5118ce4152e9f3404e109dd3049e6eacd253
Mitch Mills <mitchmillsgcp@gmail.com>
2024-03-01 08:26:32 -0600

    PEDAC practice

---
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -0,0 +1,24 @@
+###
+
+
+
+
+### 1.6 Right Triangles
+# def display_triangle(size, right_angle)
+#   vertical, horizontal = right_angle.split
+
+#   lines = (1..size).to_a
+#   stars = lines.map { |reps| '*' * reps }
+#   spaces = lines.map { |reps| ' ' * (reps - 1) }.reverse
+#   triangle = stars.zip(spaces).map(&:join)
+
+#   triangle.reverse! if vertical == 'upper'
+#   triangle.map!(&:reverse) if horizontal == 'right'
+#   puts triangle
+# end
+
+# # tests
+# display_triangle(5, 'upper left')
+# display_triangle(5, 'upper right')
+# display_triangle(5, 'lower left')
+# display_triangle(5, 'lower right')
