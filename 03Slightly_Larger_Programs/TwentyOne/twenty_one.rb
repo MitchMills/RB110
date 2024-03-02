3a3967ff8f56ce7c9198b2360e557de57b5077af
Mitch Mills <mitchmillsgcp@gmail.com>
2024-03-02 12:16:38 -0600

    Worked on scoring methods

--- 03Slightly_Larger_Programs/TwentyOne/twenty_one.rb
+++ 03Slightly_Larger_Programs/TwentyOne/twenty_one.rb
@@ -1,8 +1,12 @@
-SUITS = %w[Clubs Diamonds Hearts Spades]
-FACE_VALUES = ('2'..'10').to_a + %w[Jack Queen King Ace]
+SUITS = %w(Clubs Diamonds Hearts Spades)
+FACE_VALUES = ('2'..'10').to_a + %w(Jack Queen King Ace)
 ONE_DECK = FACE_VALUES.each_with_object([]) do |value, deck|
   SUITS.each { |suit| deck << "#{value} of #{suit}" }
 end
+
+BLACKJACK = 21
+BUSTED = 22
+
 
 # general methods
 def prompt(message, action = :puts)
@@ -20,17 +24,16 @@ def initialize_deck(game_data, number = 1)
 end
 
 def initialize_hands(game_data)
-  game_data[:hands] = {player: [], dealer: []}
+  game_data[:hands] = { player: [], dealer: [] }
 end
 
-def deal_one_card(deck, hand)
-  hand << deck.pop
+def deal_one_card(deck, person)
+  person << deck.shift
 end
 
-def deal_initial_hands(deck, hands)
+def deal_starting_hands(deck, hands)
   2.times do
-    deal_one_card(deck, hands[:player])
-    deal_one_card(deck, hands[:dealer])
+    [:player, :dealer].each { |person| deal_one_card(deck, hands[person]) }
   end
 end
 
@@ -38,7 +41,13 @@ end
 # score methods
 def hand_score(hand)
   raw_score = get_raw_score(hand)
-  adjust_score(hand, raw_score) if raw_score > 21
+  adjust_score(hand, raw_score)
+end
+
+def visible_score(hand)
+  hand -= [hand[1]]
+  raw_score = get_raw_score(hand)
+  adjust_score(hand, raw_score)
 end
 
 def get_raw_score(hand)
@@ -49,29 +58,57 @@ def get_raw_score(hand)
 end
 
 def face_card_value(value)
-  %w[Jack Queen King].include?(value) ? 10 : 11
+  %w(Jack Queen King).include?(value) ? 10 : 11
 end
 
 def adjust_score(hand, score)
-  aces = hand.map { |card| card.split.first }.count('Ace')
-  aces.times { score -= 10 if score > 21 }
+  number_of_aces = hand.map { |card| card.split.first }.count('Ace')
+  number_of_aces.times { score -= 10 if score > 21 }
   score
 end
 
 
+# hand display methods
+def display_both_hands(game_data)
+  game_data[:hands].keys.each { |person| display_hand(game_data, person) }
+end
+
+def display_hand_info(game_data, person)
+  title = person = :player ? "YOUR" : "DEALER'S"
+  puts "#{title} HAND:"
+  display_hand(game_data, person)
+  display_score(game_data, person)
+  blank_line
+end
+
+def display_hand(game_data, person)
+  hand = game_data[:hands][person]
+  hand[1] = "Face-down Card" if person == :dealer
+  hand.each { |card| puts "  #{card}" }
+end
+
+def display_score(game_data, person)
+  hand = game_data[:hands][person]
+  score = person == :player ? hand_score(hand) : visible_score(hand)
+end
+
+
 # tests
-hand = ["King of Hearts", "3 of Spades", "9 of Hearts"]
+hand = ["Ace of Hearts", "3 of Spades", "Ace of Hearts"]
 p hand_score(hand)
 
+# system('clear')
 # game_data = {}
 # deck = initialize_deck(game_data)
 # p deck
 # puts
 # hands = initialize_hands(game_data)
-# deal_initial_hands(deck, hands)
+# deal_starting_hands(deck, hands)
 # p game_data[:hands]
 # puts
 # p deck
+# puts
+# display_both_hands(game_data)
 
 
 
