5c56281ac5aba35061b6f337c09f27b430610e54
Mitch Mills <mitchmillsgcp@gmail.com>
2024-03-02 08:16:44 -0600

    Worked on scoring methods

--- 03Slightly_Larger_Programs/TwentyOne/cards.rb
+++ 03Slightly_Larger_Programs/TwentyOne/cards.rb
@@ -1,10 +1,10 @@
 SUITS = %w[Clubs Diamonds Hearts Spades]
 FACE_VALUES = ('2'..'10').to_a + %w[Jack Queen King Ace]
 
-DECK = FACE_VALUES.each_with_object([]) do |value, deck|
-  SUITS.each { |suit| deck << "#{value} of #{suit}" }
-end
+# DECK = FACE_VALUES.each_with_object([]) do |value, deck|
+#   SUITS.each { |suit| deck << "#{value} of #{suit}" }
+# end
 
-DECK.shuffle!
-p DECK
-p DECK.pop.split.first
+# p FACE_VALUES.product(SUITS).map { |card| card.join(' of ') }
+
+p (FACE_VALUES * 4).zip(SUITS * 13).map { |card| card.join(' of ') }.size
--- 03Slightly_Larger_Programs/TwentyOne/twenty_one.rb
+++ 03Slightly_Larger_Programs/TwentyOne/twenty_one.rb
@@ -1,6 +1,5 @@
 SUITS = %w[Clubs Diamonds Hearts Spades]
 FACE_VALUES = ('2'..'10').to_a + %w[Jack Queen King Ace]
-
 ONE_DECK = FACE_VALUES.each_with_object([]) do |value, deck|
   SUITS.each { |suit| deck << "#{value} of #{suit}" }
 end
@@ -10,36 +9,73 @@ def prompt(message, action = :puts)
   action == :print ? print("=> #{message}") : puts("=> #{message}")
 end
 
-# deck methods
-def initialize_deck(number = 1)
-  (ONE_DECK * number).shuffle
+def blank_line(number = 1)
+  number.times { puts }
 end
 
-def deal_initial_hands(deck)
-  hands = {player: [], dealer: []}
-  2.times do
-    hands[:player][:hand] << deck.pop
-    hands[:dealer][:hand] << deck.pop
-  end
-  hands
+
+# deck methods
+def initialize_deck(game_data, number = 1)
+  game_data[:deck] = (ONE_DECK * number).shuffle
 end
+
+def initialize_hands(game_data)
+  game_data[:hands] = {player: [], dealer: []}
+end
+
+def deal_one_card(deck, hand)
+  hand << deck.pop
+end
+
+def deal_initial_hands(deck, hands)
+  2.times do
+    deal_one_card(deck, hands[:player])
+    deal_one_card(deck, hands[:dealer])
+  end
+end
+
 
 # score methods
 def hand_score(hand)
+  raw_score = get_raw_score(hand)
+  adjust_score(hand, raw_score) if raw_score > 21
+end
+
+def get_raw_score(hand)
   values = hand.map { |card| card.split.first }
   values.map do |value|
     value.to_i > 0 ? value.to_i : face_card_value(value)
-  end
+  end.sum
+end
+
+def face_card_value(value)
+  %w[Jack Queen King].include?(value) ? 10 : 11
+end
+
+def adjust_score(hand, score)
+  aces = hand.map { |card| card.split.first }.count('Ace')
+  aces.times { score -= 10 if score > 21 }
+  score
 end
 
 
-# deck = initialize_deck
-# hands = deal_initial_hands(deck)
+# tests
+hand = ["King of Hearts", "3 of Spades", "9 of Hearts"]
+p hand_score(hand)
 
-hands = {:player=>["6 of Diamonds", "4 of Diamonds"], :dealer=>["7 of Hearts", "2 of Diamonds"]}
+# game_data = {}
+# deck = initialize_deck(game_data)
+# p deck
+# puts
+# hands = initialize_hands(game_data)
+# deal_initial_hands(deck, hands)
+# p game_data[:hands]
+# puts
+# p deck
 
-p hand_score(hands[:player])
 
+
+# OUTLINE
 # 1. Initialize deck
 # 2. Deal cards to player and dealer
 
