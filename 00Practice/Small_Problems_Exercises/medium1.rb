4e01964a500ee005df5907a797ccdab9615ee72d
Mitch Mills <mitchmillsgcp@gmail.com>
2024-03-24 09:19:04 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/medium1.rb
+++ 00Practice/Small_Problems_Exercises/medium1.rb
@@ -1,3 +1,6 @@
+
+
+
 ### 3.2 ROTATION III
 =begin
 If you take a number like 735291, and rotate it to the left, you get 352917. If you now keep the first digit fixed in place, and rotate the remaining digits, you get 329175. Keep the first 2 digits fixed in place and rotate again to 321759. Keep the first 3 digits fixed in place and rotate again to get 321597. Finally, keep the first 4 digits fixed in place and rotate the final 2 digits to get 321579. The resulting number is called the maximum rotation of the original number.
@@ -49,21 +52,21 @@ ALGORITHM
 
 =end
 
-def max_rotation(number)
-  digits = number.digits.reverse
-  last_index = digits.size - 2
-  (0..last_index).each do |index|
-    removed = digits.delete_at(index)
-    digits << removed
-  end
-  digits.join.to_i
-end
+# def max_rotation(number)
+#   digits = number.digits.reverse
+#   last_index = digits.size - 2
+#   (0..last_index).each do |index|
+#     removed = digits.delete_at(index)
+#     digits << removed
+#   end
+#   digits.join.to_i
+# end
 
-p max_rotation(735291) == 321579
-p max_rotation(3) == 3
-p max_rotation(35) == 53
-p max_rotation(105) == 15 # the leading zero gets dropped
-p max_rotation(8_703_529_146) == 7_321_609_845
+# p max_rotation(735291) == 321579
+# p max_rotation(3) == 3
+# p max_rotation(35) == 53
+# p max_rotation(105) == 15 # the leading zero gets dropped
+# p max_rotation(8_703_529_146) == 7_321_609_845
 
 
 ### 2.2 ROTATION II
