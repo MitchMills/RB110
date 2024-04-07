5859ca65a7478dce019c64a63b2b83f0f2caa4e8
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-07 07:02:03 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/medium2.rb
+++ 00Practice/Small_Problems_Exercises/medium2.rb
@@ -1,3 +1,25 @@
+### 5 TRIANGLE SIDES
+=begin
+A triangle is classified as follows:
+
+    equilateral All 3 sides are of equal length
+    isosceles 2 sides are of equal length, while the 3rd is different
+    scalene All 3 sides are of different length
+
+To be a valid triangle, the sum of the lengths of the two shortest sides must be greater than the length of the longest side, and all sides must have lengths greater than 0: if either of these conditions is not satisfied, the triangle is invalid.
+
+Write a method that takes the lengths of the 3 sides of a triangle as arguments, and returns a symbol :equilateral, :isosceles, :scalene, or :invalid depending on whether the triangle is equilateral, isosceles, scalene, or invalid.
+=end
+
+
+p triangle(3, 3, 3) == :equilateral
+p triangle(3, 3, 1.5) == :isosceles
+p triangle(3, 4, 5) == :scalene
+p triangle(0, 3, 3) == :invalid
+p triangle(3, 1, 1) == :invalid
+
+
+
 ### 4 MATCHING PARENTHESES
 =begin
 Write a method that takes a string as an argument, and returns true if all parentheses in the string are properly balanced, false otherwise. To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
@@ -37,26 +59,26 @@ ALGORITHM
 - return true if counter == 0 at end, false otherwise
 =end
 
-def balanced?(string)
-  balance = 0
-  string.chars.each do |character|
-    balance += 1 if character == '('
-    balance -= 1 if character == ')'
-    break if balance < 0
-  end
-  balance == 0
-end
+# def balanced?(string)
+#   balance = 0
+#   string.chars.each do |character|
+#     balance += 1 if character == '('
+#     balance -= 1 if character == ')'
+#     break if balance < 0
+#   end
+#   balance == 0
+# end
 
 
-p balanced?('What (is) this?') == true
-p balanced?('What is) this?') == false
-p balanced?('What (is this?') == false
-p balanced?('((What) (is this))?') == true
-p balanced?('((What)) (is this))?') == false
-p balanced?('Hey!') == true
-p balanced?(')Hey!(') == false
-p balanced?('What ((is))) up(') == false
-p balanced?('What ())(is() up') == false
+# p balanced?('What (is) this?') == true
+# p balanced?('What is) this?') == false
+# p balanced?('What (is this?') == false
+# p balanced?('((What) (is this))?') == true
+# p balanced?('((What)) (is this))?') == false
+# p balanced?('Hey!') == true
+# p balanced?(')Hey!(') == false
+# p balanced?('What ((is))) up(') == false
+# p balanced?('What ())(is() up') == false
 
 
 ### 3 LETTERCASE PERCENTAGE
