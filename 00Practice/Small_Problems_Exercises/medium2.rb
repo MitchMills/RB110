359a42af1fcc9d84db20b7724a6cec1e0272a762
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-14 09:01:52 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/medium2.rb
+++ 00Practice/Small_Problems_Exercises/medium2.rb
@@ -1,5 +1,65 @@
-### 10
+### 11 IS IT PRIME?
 
+
+
+
+### 10 SUM SQUARE
+=begin
+Write a method that computes the difference between the square of the sum of the first n positive integers and the sum of the squares of the first n positive integers.
+
+PROBLEM
+input: integer
+  - positive
+  - represents the number of positive integers to use in the calculation
+    - e.g. 3 -> 1, 2, 3
+
+output: integer
+  - difference between:
+    - all numbers added together then squared: (1 + 2 + 3)**2
+    - each number squared then all added together: 1**2 + 2**2 + 3**2
+
+EXAMPLES
+
+DATA STRUCTURES
+- needs:
+  - list of all positive integers up to input integer
+    - sum squared
+    - each squared then summed
+
+- input: integer
+  - range from 1 up to integer
+  - sum of that range, squared
+  - each number in the range squared, then summer
+  - difference = output
+- output: integer
+
+ALGORITHM
+- get the sum of all integers and square it
+  - create a range from 1 up to the input integer
+    - get the sum
+- square every integer and sum the squares
+  - create a range from 1 up to the input integer (or re-use range above)
+    - for each number in the range, square it and add it to a total
+- get the difference and return it
+  - subtract the second number from the first
+=end
+
+# def sum_square_difference(number)
+#   sum_squared(number) - sum_of_squares(number)
+# end
+
+# def sum_squared(number)
+#   (1..number).sum**2
+# end
+
+# def sum_of_squares(number)
+#   (1..number).sum(&:abs2)
+# end
+
+# p sum_square_difference(3) == 22 # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+# p sum_square_difference(10) == 2640
+# p sum_square_difference(1) == 0
+# p sum_square_difference(100) == 25164150
 
 
 
