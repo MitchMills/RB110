a423d904faefd894b2d02e7282f5458fffd27e6a
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-15 07:28:19 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,33 +1,90 @@
-### 2 ODD
+### STRINGY STRINGS
 =begin
-Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
 
 PROBLEM
 input: integer
-  - may be negative, zero, or positive
-  - valid integer value
-output: boolean
-  - true if input integer is odd, false otherwise
+  - positive
+  - represents desired length of output string
+
+output: string
+  - length = input integer
+  - alternating 1s and 0s, starting with 1
 
 EXAMPLES
 
 DATA STRUCTURES
 - needs:
+  - way to determine whether next character is 1 or 0
 
 - input: integer
+  - empty string to add 1s and 0s to
+  - boolean toggle to determine 1s or 0s
+    - true = 1, false = 0
+- output: string
 
-- output: boolean
-
-ALGORITHM
+- ALGORITHM
+  - create an empty string
+  - create a variable to control adding a 1 or 0: add_one
+    - set it to true
+  - for the desired length
+    - add a character to the empty string
+      - if add_one is true, add a '1'
+      - else add a '0'
+  - return the string
 =end
 
-def is_odd?(number)
-  number % 2 != 0
+# def stringy(length)
+#   string = ''
+#   add_one = true
+#   length.times do
+#     next_character = add_one ? '1' : '0'
+#     string << next_character
+#     add_one = !add_one
+#   end
+#   string
+# end
+
+# def stringy(length)
+#   string = ''
+#   length.times do |index|
+#     next_character = index.even? ? '1' : '0'
+#     string << next_character
+#   end
+#   string
+# end
+
+# def stringy(length, first_character = '1')
+# string = ''
+# characters = first_character == '1' ? ['1', '0'] : ['0', '1']
+
+# length.times do |index|
+#   next_character = index.even? ? characters[0] : characters[1]
+#   string << next_character
+# end
+# string
+# end
+
+# def stringy(length)
+#   (1..length).map { |place| place.odd? ? '1' : '0' }.join
+# end
+
+# def stringy(length, start_with_one = true)
+#   characters = start_with_one ? %w(1 0) : %w(0 1)
+#   (1..length).map { |place| place.odd? ? characters[0] : characters[1] }.join
+# end
+
+# def stringy(length, first = 1)
+#   second = first == 1 ? 0 : 1
+#   Array.new(length) { |index| index.even? ? first : second }.join
+# end
+
+def stringy(length, first = 1)
+  Array.new(length) { |index| index.even? ? first : 1 - first }.join
 end
 
-p is_odd?(2)    == false
-p is_odd?(5)    == true
-p is_odd?(-17)  == true
-p is_odd?(-8)   == false
-p is_odd?(0)    == false
-p is_odd?(7)    == true
+
+p stringy(6) == '101010'
+p stringy(9) == '101010101'
+p stringy(4) == '1010'
+p stringy(7) == '1010101'
