20288249dffaed1ed142add2c93fab6565cf809f
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-16 17:51:28 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,59 +1,36 @@
-# frozen_string_literal: true
+PERCENT_DIVISOR = 100.0
 
-system('clear')
-
-SQUARE_FEET_PER_SQUARE_METER = 10.7639
-SQUARE_INCHES_PER_SQUARE_METER = 1_550.0031
-SQUARE_CENTIMETERS_PER_SQUARE_METER = 10_000
-
-SQUARE_METERS_PER_SQUARE_FOOT = 0.0929
-SQUARE_INCHES_PER_SQUARE_FOOT = 144
-SQUARE_CENTIMETERS_PER_SQUARE_FOOT = 929.0304
-
-# keys in outer hash are starting unit; keys in inner hashes are target units
-CONVERSIONS = {
-  'square meters' => {
-    'square feet' => SQUARE_FEET_PER_SQUARE_METER,
-    'square inches' => SQUARE_INCHES_PER_SQUARE_METER,
-    'square centimeters' => SQUARE_CENTIMETERS_PER_SQUARE_METER
-  },
-  'square feet' => {
-    'square inches' => SQUARE_INCHES_PER_SQUARE_FOOT,
-    'square centimeters' => SQUARE_CENTIMETERS_PER_SQUARE_FOOT,
-    'square meters' => SQUARE_METERS_PER_SQUARE_FOOT
-  }
-}.freeze
-
-def find_area(start_unit, target_units = [])
-  dimensions = input_dimensions(start_unit)
-  area = dimensions.inject(:*)
-  display_area(area, start_unit)
-  display_conversions(area, start_unit, target_units)
+def tip_calculator
+  amounts = input_amounts
+  display_tip_and_total(amounts)
 end
 
-def input_dimensions(start_unit)
-  base_unit = start_unit.split[1]
-  %w[length width].map do |dimension|
-    print "Enter the #{dimension} of the room in #{base_unit}: "
+def input_amounts
+  ['bill', 'tip percentage'].map do |type|
+    print "What is the #{type}? "
     gets.chomp.to_f
   end
 end
 
-def display_area(area, start_unit)
-  puts "The area of the room is #{format_area(area)} #{start_unit}."
-end
-
-def format_area(area)
-  format('%.2f', area).reverse.scan(/(\d*\.\d{1,3}|\d{1,3})/).join(',').reverse
-end
-
-def display_conversions(area, start_unit, target_units)
-  target_units.each do |target_unit|
-    converter = CONVERSIONS[start_unit][target_unit]
-    converted_area = area * converter
-    puts "(#{format_area(converted_area)} #{target_unit})"
+def display_tip_and_total(amounts)
+  puts
+  tip_and_total = calculate_tip_and_total(amounts)
+  tip_and_total.each do |label, amount|
+    puts "The #{label} is #{format_currency(amount)}"
   end
 end
 
-# find_area('square meters', ['square feet'])
-find_area('square feet', ['square inches', 'square centimeters'])
+def calculate_tip_and_total(amounts)
+  bill, tip_percentage = amounts
+  tip = bill * (tip_percentage / PERCENT_DIVISOR)
+  total = bill + tip
+  %w[tip total].zip([tip, total]).to_h
+end
+
+def format_currency(amount)
+  basic = format('%.2f', amount)
+  with_commas = basic.reverse.scan(/(\d*\.\d{1,3}|\d{1,3})/).join(',').reverse
+  with_commas.prepend('$')
+end
+
+tip_calculator
