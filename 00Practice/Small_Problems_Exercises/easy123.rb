373d449c0151f87eb87670f2867594bd35a7b84a
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-18 12:43:50 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,30 +1,17 @@
-def retirement_calculator
-  age_info = collect_age_info
-  retirement_info = calculate_retirement_info(age_info)
-  display(retirement_info)
+### ODD NUMBERS
+=begin
+Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+PROBLEM
+input: integer
+
+output: integers displayed to screen
+  - only odd numbers between 1 and input integer, inclusive
+  - each number on a separate line
+=end
+
+def display_odd_numbers(limit)
+  (1..limit).step(2) { |num| puts num }
 end
 
-def collect_age_info
-  questions = ["What is your age? ", "At what age would you like to retire? "]
-  questions.map do |question|
-    print question
-    gets.chomp.to_i
-  end
-end
-
-def calculate_retirement_info(user_info)
-  ages = user_info
-  years_to_work = ages.reverse.inject(:-)
-  current_year = Time.now.year
-  retirement_year = current_year + years_to_work
-  [current_year, retirement_year, years_to_work]
-end
-
-def display(retirement_info)
-  current_year, retirement_year, years_to_work = retirement_info
-  puts
-  puts "It's #{current_year}. You will retire in #{retirement_year}."
-  puts "You have only #{years_to_work} years of work to go!"
-end
-
-retirement_calculator
+display_odd_numbers(21)
