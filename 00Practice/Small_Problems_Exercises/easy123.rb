dc9509d9155a031810d95cf5150ce3b7bdc6b254
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-18 08:55:49 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -12,12 +12,17 @@ def input_ages
 end
 
 def display_retirement_info(ages)
-  years_to_work = ages.reverse.inject(:-)
-  current_year = Time.now.year
-  retirement_year = current_year + years_to_work
+  current_year, retirement_year, years_to_work = retirement_info(ages)
   puts
   puts "It's #{current_year}. You will retire in #{retirement_year}."
   puts "You have only #{years_to_work} years of work to go!"
+end
+
+def retirement_info(ages)
+  years_to_work = ages.reverse.inject(:-)
+  current_year = Time.now.year
+  retirement_year = current_year + years_to_work
+  [current_year, retirement_year, years_to_work]
 end
 
 retirement_calculator
