b964b2e745e389e756f7e26af2338562d473ca6e
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-20 17:13:22 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,45 +1,59 @@
-def integer_arithmetic
-  integers = request_integers
-  results = calculate_results(integers)
-  display_results(integers, results)
+def arithmetic
+  numbers = request_numbers
+  results = calculate_results(numbers)
+  display_results(numbers, results)
 end
 
-def request_integers
+def request_numbers
   %w(first second).map { |place| enter_number(place) }
 end
 
 def enter_number(place)
   loop do
-    print "==> Enter the #{place} positive integer: "
+    print "==> Enter the #{place} number: "
     entry = gets.chomp
-    return entry.to_i if valid_entry?(entry)
+    return entry.to_f if valid_entry?(entry)
 
     invalid_entry(entry)
   end
 end
 
 def valid_entry?(entry)
-  entry.to_i.to_s == entry && entry.to_i.positive?
+  entry.to_f.to_s == entry || entry.to_i.to_s == entry
 end
 
 def invalid_entry(entry)
-  puts
-  puts "==> I'm sorry, '#{entry}' is not a positive integer. Please try again."
+  puts "==> Sorry, '#{entry}' is not a number. Please try again."
   puts
 end
 
-def calculate_results(integers)
+def calculate_results(numbers)
   operators = %i(+ - * / % **)
-  results = operators.map { |operator| integers.inject(operator) }
+  results = operators.map do |operator|
+    next "error: can't divide by zero" if zero_error(numbers, operator)
+    numbers.inject(operator)
+  end
   operators.zip(results).to_h
 end
 
-def display_results(integers, results)
+def zero_error(numbers, operator)
+  numbers.last == 0 && %i(/ %).include?(operator)
+end
+
+def display_results(numbers, results)
   puts
-  number1, number2 = integers
+  number1, number2 = numbers.map { |number| format_number(number) }
   results.each do |operation, result|
+    result = format_number(result) unless result.instance_of?(String)
     puts "==> #{number1} #{operation} #{number2} = #{result}"
   end
 end
 
-integer_arithmetic
+def format_number(number)
+  basic = format('%.2f', number)
+  with_commas = basic.reverse.scan(/(\d*\.\d{1,3}|\d{1,3})/).join(',').reverse
+  final = with_commas.gsub('.00', '')
+  number < 0 ? final.prepend('-') : final
+end
+
+arithmetic
