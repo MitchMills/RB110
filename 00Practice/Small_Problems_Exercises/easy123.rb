c1ac494a1f360153fd9872990e8a4d65c4eb1107
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-20 09:03:27 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,45 +1,45 @@
-def inclusion_checker
-  numbers = prompt_numbers
-  display_result(numbers)
+def integer_arithmetic
+  integers = request_integers
+  results = calculate_results(integers)
+  display_results(integers, results)
 end
 
-def prompt_numbers
-  places = %w(1st 2nd 3rd 4th 5th last)
-  places.map { |place| enter_number(place) }
+def request_integers
+  %w(first second).map { |place| enter_number(place) }
 end
 
 def enter_number(place)
   loop do
-    print "==> Enter the #{place} integer: "
+    print "==> Enter the #{place} positive integer: "
     entry = gets.chomp
-    puts
-    return entry.to_i if valid_integer?(entry)
+    return entry.to_i if valid_entry?(entry)
 
-    invalid_entry
+    invalid_entry(entry)
   end
 end
 
-def valid_integer?(entry)
-  entry.to_i.to_s == entry
+def valid_entry?(entry)
+  entry.to_i.to_s == entry && entry.to_i.positive?
 end
 
-def invalid_entry
-  puts "Sorry, that's not an integer. Try again."
+def invalid_entry(entry)
+  puts
+  puts "==> I'm sorry, '#{entry}' is not a positive integer. Please try again."
   puts
 end
 
-def display_result(numbers)
-  last = numbers.pop
-  verb_phrase = verb_phrase(numbers, last)
-  puts "The number #{last} #{verb_phrase} in #{numbers}."
+def calculate_results(integers)
+  operators = %i(+ - * / % **)
+  results = operators.map { |operator| integers.inject(operator) }
+  operators.zip(results).to_h
 end
 
-def verb_phrase(numbers, last)
-  included?(numbers, last) ? "appears" : "does not appear"
+def display_results(integers, results)
+  puts
+  number1, number2 = integers
+  results.each do |operation, result|
+    puts "==> #{number1} #{operation} #{number2} = #{result}"
+  end
 end
 
-def included?(numbers, last)
-  numbers.include?(last)
-end
-
-inclusion_checker
+integer_arithmetic
