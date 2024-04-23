722b68a6216d5048db4a4bbbfb5d183dcf0c7f08
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-23 13:08:14 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,36 +1,24 @@
-### EASY 4.1 SHORT LONG SHORT
-=begin
-Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the result of concatenating the shorter string, the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
-
-PROBLEM
-input: two strings
-  - two different lengths
-  - one string may be empty
-output: string
-  - concatentated input strings: shorter + longer + shorter
-
-EXAMPLES
-
-DATA STRUCTURES
-- needs:
-  - way to determine which string is shortest / longest
-
-- input: 2 strings
-  - array of both strings sorted by length
-- output: string
-
-ALGORITHM
-- create an array containing both input strings
-- sort the array by length
-- concatenate strings: short + long + short
-- return that string
-=end
-
-def short_long_short(string1, string2)
-  short, long = [string1, string2].sort_by(&:size)
-  short + long + short
+def century(year)
+  century = get_century(year)
+  format_century(century)
 end
 
-p short_long_short('abc', 'defgh') == "abcdefghabc"
-p short_long_short('abcde', 'fgh') == "fghabcdefgh"
-p short_long_short('', 'xyz') == "xyz"
+def get_century(year)
+  century, remainder = year.divmod(100)
+  remainder == 0 ? century : century + 1
+end
+
+def format_century(century)
+  quotient, remainder = century.divmod(10)
+  suffix = quotient % 10 == 1 ? 'th' : get_suffix(remainder)
+  century.to_s + suffix
+end
+
+def get_suffix(remainder)
+  case remainder
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
+  end
+end
