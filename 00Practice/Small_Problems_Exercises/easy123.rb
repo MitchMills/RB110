c0ac2cbdb6eb73f8498d1cab74729b8a81a6dc43
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-24 08:35:10 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,21 +1,17 @@
 def century(year)
-  century = get_century(year)
-  format_century(century)
-end
-
-def get_century(year)
-  century, remainder = year.divmod(100)
-  remainder == 0 ? century : century + 1
-end
-
-def format_century(century)
-  quotient, remainder = century.divmod(10)
-  suffix = quotient % 10 == 1 ? 'th' : get_suffix(remainder)
+  century = determine_century(year)
+  suffix = determine_suffix(century)
   century.to_s + suffix
 end
 
-def get_suffix(remainder)
-  case remainder
+def determine_century(year)
+  quotient, remainder = year.divmod(100)
+  remainder == 0 ? quotient : quotient + 1
+end
+
+def determine_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  case century % 10
   when 1 then 'st'
   when 2 then 'nd'
   when 3 then 'rd'
