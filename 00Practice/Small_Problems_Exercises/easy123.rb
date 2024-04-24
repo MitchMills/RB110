253190bbb35a13638eb987184ffd9e2d75bf4080
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-24 11:30:06 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,64 +1,40 @@
-=begin 8:36
-In the modern era under the Gregorian Calendar, leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100. If the year is evenly divisible by 100, then it is not a leap year unless the year is evenly divisible by 400.
+=begin
+A continuation of the previous exercise.
 
-Assume this rule is good for any year greater than year 0. Write a method that takes any year greater than 0 as input, and returns true if the year is a leap year, or false if it is not a leap year.
+The British Empire adopted the Gregorian Calendar in 1752, which was a leap year. Prior to 1752, the Julian Calendar was used. Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+
+Using this information, update the method from the previous exercise to determine leap years both before and after 1752.
 
 PROBLEM
 input: integer
-  - represents a year
+  - represents year
 output: boolean
   - true if input year is a leap year, false otherwise
-  - leap year
-    - evenly divisible by 4
-      - unless also evenly divisible by 100
-        - not a leap year unless also evenly divisible by 400
+  - leap year:
+    - before 1752: (Julian)
+      - any year evenly divisible by 4
+    - 1752 and after: (Gregorian)
+      - divisible by 4 unless (divisible by 100 and not divisible by 400)
 
 EXAMPLES
 
 DATA STRUCTURES
 - needs
-  - leap year logic
-    - year % 400 == 0 &&
-    - year % 100 == 0 &&
-    - year % 4 == 0
+  - whether to use Julian or Gregorian calculation
 
-ALGORITHM
-- if evenly divisible by 4
-  - if yes, check if evenly divisible by 100
-    - if yes, check if evenly divisible by 400
-      - if yes, true
-    - if no, false
-  - if no, then true
-- if no, then false
+- input: year
+- output: boolean
 =end
 
-# def leap_year?(year)
-#   if year % 4 == 0
-#     if year % 100 == 0
-#       if year % 400 == 0
-#         true
-#       else
-#         false
-#       end
-#     else
-#       true
-#     end
-#   else
-#     false
-#   end
-# end
-
-# def leap_year?(year)
-#   year % 4 == 0 ? check_exceptions(year) : false
-# end
-
-# def check_exceptions(year)
-#   year % 100 == 0 ? year % 400 == 0 : true
-# end
-
 def leap_year?(year)
-  # year % 4 == 0 && year % 100 != 0 ? true : year % 400 == 0
-  # year % 4 == 0 ? year % 100 != 0 || year % 400 == 0 : false
+  year < 1752 ? julian_leap_year?(year) : gregorian_leap_year?(year)
+end
+
+def julian_leap_year?(year)
+  year % 4 == 0
+end
+
+def gregorian_leap_year?(year)
   year % 100 == 0 ? year % 400 == 0 : year % 4 == 0
 end
 
@@ -71,7 +47,7 @@ p leap_year?(240001) == false
 p leap_year?(2000) == true
 p leap_year?(1900) == false
 p leap_year?(1752) == true
-p leap_year?(1700) == false
+p leap_year?(1700) == true
 p leap_year?(1) == false
-p leap_year?(100) == false
+p leap_year?(100) == true
 p leap_year?(400) == true
