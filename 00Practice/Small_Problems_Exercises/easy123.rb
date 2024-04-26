3b0ed4478f8a46f45d43debcb692bc4db6cb86a8
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-26 12:38:38 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,17 +1,31 @@
 DIGITS = ('0'..'9').to_a + ('a'..'f').to_a
 
-def string_to_integer(string, base)
-  return base_error_message if base_error(string, base)
+def string_to_integer(string, base = 10)
+  return error_message(string, base) if input_error?(string, base)
   digits = convert_to_digits(string)
   combine_digits(digits, base)
 end
 
-def base_error_message
-  "Cannot convert to a decimal number: input string contains letters."
+def error_message(string, base)
+  error_type = input_error(string, base)
+  error_messages = {
+    wrong_base: "Error: base must be either 10 or 16.",
+    string_error: "Error: input string contains invalid characters.",
+    base10: "Error: cannot convert to a decimal number: input string contains letters."
+  }
+  error_messages[error_type]
 end
 
-def base_error(string, base)
-  base == 10 && string.downcase.chars.any? { |char| ('a'..'z').include?(char) }
+def input_error?(string, base)
+  !!input_error(string, base)
+end
+
+def input_error(string, base)
+  chars = string.downcase.chars
+  return :wrong_base unless [10, 16].include?(base)
+  return :string_error unless chars.all? { |char| DIGITS.include?(char) }
+  return :base10 if base == 10 && chars.any? { |char| 'abcdef'.include?(char) }
+  nil
 end
 
 def convert_to_digits(string)
