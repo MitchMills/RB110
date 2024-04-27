5bdd07f53054631717d52a469c7982f78c5cf271
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-27 12:29:43 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy123.rb
+++ 00Practice/Small_Problems_Exercises/easy123.rb
@@ -1,37 +1,72 @@
-def string_to_signed_integer(string)
-  digits = get_digits(string)
-  combine_digits(string.chr, digits)
-end
+=begin
+In the previous two exercises, you developed methods that convert simple numeric strings to signed Integers. In this exercise and the next, you're going to reverse those methods.
 
-def get_digits(string)
-  all_digits = ('0'..'9').to_a
-  numbers = string.chars.select { |char| all_digits.include?(char) }
-  numbers.map { |number| all_digits.index(number) }
-end
+Write a method that takes a positive integer or zero, and converts it to a string representation.
 
-def combine_digits(first_character, digits)
-  result = digits.reverse.map.with_index { |digit, idx| digit * (10**idx) }.sum
-  first_character == '-' ? -result : result
-end
+You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s, String(), Kernel#format, etc. Your method should do this the old-fashioned way and construct the string by analyzing and manipulating the number.
 
-# def string_to_signed_integer(string)
-#   unsigned_string = %w(- +).include?(string[0]) ? string[1..-1] : string
-#   number = string_to_integer(unsigned_string)
-#   string[0] == '-' ? -number : number
+PROBLEM 11:36
+input: integer
+  - positive or zero
+
+output: string
+  - represents same number as input integer
+
+EXAMPLES
+
+DATA STRUCTURES
+- needs:
+  - way to access each digit of input integer individually
+  - way to convert digit into string
+
+- input: integer
+  - array of individual digits from input integer
+  - transformed into strings
+  - joined into a single string
+- output: string
+
+ALGORITHM
+- get individual digits of input integer
+  - create an array of each digit from input integer
+- convert digits into string equivalents
+  - create a hash with digits as keys and strings as values
+  - transform array using hash
+- create output string
+  - join elements of hash and return them
+=end
+
+DIGITS_TO_STRINGS = ('0'..'9').to_a
+
+# def integer_to_string(integer)
+#   digits = integer.digits.reverse
+#   digits.map { |digit| DIGITS_TO_STRINGS[digit] }.join
 # end
 
-# DIGITS = {
-#   '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
-#   '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
-# }
-
-# def string_to_integer(string)
-#   digits = string.chars.map { |char| DIGITS[char] }
-#   value = 0
-#   digits.each { |digit| value = (10 * value) + digit }
-#   value
+# def integer_to_string(integer)
+#   digits = separate_digits(integer)
+#   digits.map { |digit| DIGITS_TO_STRINGS[digit] }.join
 # end
 
-p string_to_signed_integer('4321') #== 4321
-p string_to_signed_integer('-570') #== -570
-p string_to_signed_integer('+100') #== 100
+# def separate_digits(integer)
+#   digits = []
+#   loop do
+#     integer, remainder = integer.divmod(10)
+#     digits.prepend(remainder)
+#     break if integer == 0
+#   end
+#   digits
+# end
+
+def integer_to_string(integer)
+  result = ''
+  loop do
+    integer, remainder = integer.divmod(10)
+    result.prepend(DIGITS_TO_STRINGS[remainder])
+    break if integer == 0
+  end
+  result
+end
+
+p integer_to_string(4321) #== '4321'
+p integer_to_string(0) #== '0'
+p integer_to_string(5000) #== '5000'
