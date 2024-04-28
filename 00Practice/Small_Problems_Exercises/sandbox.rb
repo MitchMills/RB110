d365b3e233f2afed48828fa7b8056e426559a798
Mitch Mills <mitchmillsgcp@gmail.com>
2024-04-28 11:35:32 -0500

    Nested collections practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,42 +1,18 @@
-STRING_INTEGERS = ('0'..'9').to_a
-
-def signed_integer_to_string1(integer)
-  number = integer.abs
-  result = ''
-  loop do
-    number, remainder = number.divmod(10)
-    result.prepend(STRING_INTEGERS[remainder])
-    break if number == 0
-  end
-  return result if integer == 0
-  integer > 0 ? result.prepend('+') : result.prepend('-')
-end
-
-def signed_integer_to_string2(integer)
-  result = integer.abs.digits.reverse.join
-  return result if integer.zero?
-  integer.positive? ? result.prepend('+') : result.prepend('-')
-end
-
-def signed_integer_to_string3(integer)
-  sign = ['', '+', '-'][integer <=> 0]
-  number = integer.abs.digits.reduce('') do |result, digit|
-    result.prepend(STRING_INTEGERS[digit])
-  end
-  sign + number
-end
-
-def signed_integer_to_string4(integer)
-  sign = ['', '+', '-'][integer <=> 0]
-  number = integer.abs.digits.reverse.join
-  sign + number
-end
-
-def signed_integer_to_string5(integer)
-  number = [integer].join
-  integer > 0 ? "+#{number}" : number
-end
-
-def signed_integer_to_string6(integer)
-  integer > 0 ? "+#{integer}" : "#{integer}"
-end
+grade_book = [
+  {subject: "English",
+   grades: [
+            {grade_id: 17, type: "Exam", score: 4},
+            {grade_id: 18, type: "Quiz", score: 3.5},
+            {grade_id: 19, type: "Quiz", score: 3},
+            {grade_id: 20, type: "Quiz", score: 3.5},
+            {grade_id: 21, type: "Quiz", score: 2.5}
+          ]
+  },
+  {subject: "Band",
+   grades: [
+            {grade_id: 200, type: "Exam", score: 2},
+            {grade_id: 201, type: "Performance", score: 4},
+            {grade_id: 202, type: "Assignment", score: 4}
+          ]
+  }
+]
