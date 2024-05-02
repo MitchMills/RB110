50485df55734539deaa3da1fc2de62fbebe7ff7b
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-02 07:55:33 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,18 +1,22 @@
 MINUTES_PER_HOUR = 60
-HOURS_PER_DAY = 24
+MINUTES_PER_DAY = 1440
 
-def time_of_day(minutes)
-  hours, minutes = minutes.divmod(MINUTES_PER_HOUR)
-  hours %= HOURS_PER_DAY
-  [hours, minutes].map do |unit|
-    unit < 10 ? unit.to_s.prepend('0') : unit.to_s
-  end.join(':')
+def after_midnight(time)
+  hours, minutes = time.split(':').map(&:to_i)
+  ((hours * MINUTES_PER_HOUR) + minutes) % MINUTES_PER_DAY
 end
 
-p time_of_day(0) == "00:00"
-p time_of_day(-3) == "23:57"
-p time_of_day(35) == "00:35"
-p time_of_day(-1437) == "00:03"
-p time_of_day(3000) == "02:00"
-p time_of_day(800) == "13:20"
-p time_of_day(-4231) == "01:29"
+def before_midnight(time)
+  (MINUTES_PER_DAY - after_midnight(time)) % MINUTES_PER_DAY
+end
+
+p after_midnight('00:00') == 0
+p after_midnight('12:34') == 754
+p after_midnight('24:00') == 0
+p after_midnight('03:03') == 183
+
+p before_midnight('00:00') == 0
+p before_midnight('12:34') == 686
+p before_midnight('24:00') == 0
+p before_midnight('23:57') == 3
+p before_midnight('14:00') == 600
