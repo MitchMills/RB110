20022f65fee2b69ac35c1cbff746daa742b80d6e
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-05 16:09:59 -0500

    Loops practice

--- 01Ruby_Collections/loops/loops1.rb
+++ 01Ruby_Collections/loops/loops1.rb
@@ -5,111 +5,21 @@
 #   puts "Hello, #{friend}!"
 # end
 
-### THAT'S ODD
-# for i in 1..10
-#   next if i.even?
-#   puts i
-# end
-
-### PRINT UNTIL
-# numbers = [7, 9, 13, 25, 18]
-
-# index = 0
-# until index >= numbers.size
-#   puts numbers[index]
-#   index += 1
-# end
-
-### COUNT UP
-# count = 1
-
-# until count > 10
-#   puts count
-#   count += 1
-# end
-
-### PRINT WHILE
-# numbers = []
-
-# while numbers.size < 5
-#   numbers << rand(0..99)
-# end
-
-# puts numbers
-
-### SAY HELLO
-# say_hello = true
-# counter = 1
-
-# while say_hello
-#   puts 'Hello!'
-#   counter += 1
-#   say_hello = false if counter > 5
-# end
-
-### LOOP ON COMMAND
-# loop do
-#   puts 'Should I stop looping?'
-#   answer = gets.chomp
-#   break if answer == 'yes'
-# end
-
-### CONTROL THE LOOP
-# iterations = 1
-
-# loop do
-#   puts "Number of iterations = #{iterations}"
-#   break if iterations >= 5
-#   iterations += 1
-# end
-
-### LOOPCEPTION
-# loop do
-#   puts 'This is the outer loop.'
-
-#   loop do
-#     puts 'This is the inner loop.'
-#     break
-#   end
-#   break
-# end
-
-# puts 'This is outside all loops.'
-
-### 1 RUNAWAY LOOP
-# loop do
-#   puts 'Just keep printing...'
-#   break
-# end
-
-
-### 10 GREET YOUR FRIENDS
-# friends = ['Sarah', 'John', 'Hannah', 'Dave']
-
-# for friend in friends
-#   puts "Hello, #{friend}!"
-# end
 
 ### 9 THAT'S ODD
 # for i in 1..10
 #   puts i if i.odd?
 # end
 
+
 ### 8 PRINT UNTIL
 # numbers = [7, 9, 13, 25, 18]
-
-# until numbers.size == 0
-#   puts numbers.shift
-# end
-
 # index = 0
 
 # until index == numbers.size
 #   puts numbers[index]
 #   index += 1
 # end
-
-# p numbers
 
 ### 7 COUNT UP
 # count = 1
@@ -118,6 +28,7 @@
 #   puts count
 #   count += 1
 # end
+
 
 ### 6 PRINT WHILE
 # numbers = []
@@ -128,27 +39,29 @@
 
 # puts numbers
 
-### 5 SAY HELLO
-# say_hello = true
-
-# while say_hello
-#   5.times { puts 'Hello!' }
-#   say_hello = false
+# count = 0
+# until count == 5
+#   puts rand(100)
+#   count += 1
 # end
 
-# count = 0
+### 5 SAY HELLO
+# say_hello = true
+# counter = 1
 
 # while say_hello
 #   puts 'Hello!'
-#   count += 1
-#   say_hello = false if count >= 5
+#   counter += 1
+#   say_hello = false if counter > 5
 # end
+
 
 ### 4 LOOP ON COMMAND
 # loop do
 #   puts 'Should I stop looping?'
 #   answer = gets.chomp
 #   break if answer == 'yes'
+#   puts "Then I'll keep looping!"
 # end
 
 ### 3 CONTROL THE LOOP
@@ -156,13 +69,14 @@
 
 # loop do
 #   puts "Number of iterations = #{iterations}"
-#   break if iterations >= 5
-#   iterations +=1
+#   iterations += 1
+#   break if iterations > 5
 # end
 
 ### 2 LOOPCEPTION
 # loop do
 #   puts 'This is the outer loop.'
+
 #   loop do
 #     puts 'This is the inner loop.'
 #     break
