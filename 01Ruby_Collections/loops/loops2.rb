2d4d98784df9cf129a23d48fe52f525251fca53a
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-06 08:47:32 -0500

    Loops practice

--- 01Ruby_Collections/loops/loops2.rb
+++ 01Ruby_Collections/loops/loops2.rb
@@ -1,14 +1,14 @@
-### GREETING
-def greeting
-  puts 'Hello!'
-end
+### 10 GREETING
+# def greeting
+#   puts 'Hello!'
+# end
 
-number_of_greetings = 2
+# number_of_greetings = 2
 
-while number_of_greetings > 0
-  greeting
-  number_of_greetings -= 1
-end
+# while number_of_greetings > 0
+#   greeting
+#   number_of_greetings -= 1
+# end
 
 ### 9 FIRST TO FIVE
 # number_a = 0
@@ -19,9 +19,7 @@ end
 #   number_b += rand(2)
 #   puts "a: #{number_a}, b: #{number_b}"
 
-#   next unless number_a == 5 || number_b == 5
-#   puts "5 was reached!"
-#   break
+#   break puts "5 was reached!" if [number_a, number_b].include?(5)
 # end
 
 ### 8 ONLY EVEN
@@ -29,132 +27,29 @@ end
 
 # until number == 10
 #   number += 1
-#   next unless number.even?
+#   next if number.odd?
 #   puts number
 # end
 
 ### 7 STOP COUNTING
 # 5.times do |index|
 #   puts index
-#   break if index == 2
+#   break if index < 7
 # end
 
 ### 6 EMPTY THE ARRAY
 # names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+# while !names.empty?
+#   puts names.shift
+# end
+
+# until names.empty?
+#   puts names.shift
+# end
 
 # loop do
 #   puts names.shift
-#   break if names.empty?
-# end
-
-### 5 INSERT NUMBERS
-# numbers = []
-
-# loop do
-#   puts 'Enter any number:'
-#   input = gets.chomp.to_i
-#   numbers << input
-#   break if numbers.size >= 5
-# end
-# puts numbers
-
-### 4 GET THE SUM
-# loop do
-#   puts 'What does 2 + 2 equal?'
-#   answer = gets.chomp.to_i
-
-#   if answer == 4
-#     puts "That's correct!"
-#     break
-#   end
-
-#   puts "Wrong answer. Try again!"
-# end
-
-
-### 3 CONDITIONAL LOOP
-# process_the_loop = [true, false].sample
-# puts process_the_loop
-
-# if process_the_loop
-#   loop do
-#     puts "The loop was processed!"
-#     break
-#   end
-# else
-#   puts "The loop wasn't processed!"
-# end
-
-### 2 CATCH THE NUMBER
-# loop do
-#   number = rand(100)
-#   puts number
-#   break if (0..10).include?(number)
-# end
-
-### 1 EVEN OR ODD?
-# count = 1
-
-# loop do
-#   type = count.odd? ? 'odd' : 'even'
-#   puts "#{count} is #{type}!"
-#   count += 1
-#   break if count > 5
-# end
-
-### 10 GREETING
-# def greeting
-#   puts 'Hello!'
-# end
-
-# number_of_greetings = 2
-
-# while number_of_greetings > 0
-#   greeting
-#   number_of_greetings -=1
-# end
-
-### 9 FIRST TO FIVE
-# number_a = 0
-# number_b = 0
-
-# loop do
-#   puts "a = #{number_a += rand(2)}"
-#   puts "b = #{number_b += rand(2)}"
-#   puts
-#   next unless number_a >= 5 || number_b >= 5
-#   puts "5 was reached!"
-#   break
-# end
-
-# loop do
-#   puts "a = #{number_a += rand(2)}"
-#   puts "b = #{number_b += rand(2)}"
-#   puts
-#   break puts "5 was reached!" if number_a == 5 || number_b >= 5
-# end
-
-### 8 ONLY EVEN
-# number = 0
-
-# until number == 10
-#   number += 1
-#   next if number.even?
-#   puts number
-# end
-
-### 7 STOP COUNTING
-# 5.times do |index|
-#   puts index
-#   break if index == 2
-# end
-
-### 6 EMPTY THE ARRAY
-# names = ['Sally', 'Joe', 'Lisa', 'Henry']
-
-# loop do
-#   # puts names.shift
-#   puts names.pop
 #   break if names.empty?
 # end
 
@@ -179,6 +74,7 @@ end
 
 ### 3 CONDITIONAL LOOP
 # process_the_loop = [true, false].sample
+# p process_the_loop
 
 # if process_the_loop
 #   loop do
@@ -186,14 +82,13 @@ end
 #     break
 #   end
 # else
-#   puts "The loop wasn't processed!"
+#   puts "The loop wasn't processed"
 # end
 
 ### 2 CATCH THE NUMBER
 # loop do
 #   number = rand(100)
 #   puts number
-#   # break if number.between?(0, 10)
 #   break if (0..10).include?(number)
 # end
 
@@ -201,7 +96,8 @@ end
 # count = 1
 
 # loop do
-#   puts "#{count} is " + (count.odd? ? "odd!" : "even!")
-#   break if count >= 5
+#   break if count > 5
+#   type = count.odd? ? 'odd' : 'even'
+#   puts "#{count} is #{type}!"
 #   count += 1
 # end
