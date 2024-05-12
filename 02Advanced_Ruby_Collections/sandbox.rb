f38d8244b0e09a4c016a01304eaa77b65d7ab3bf
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-12 11:33:02 -0500

    Nested collections practice

--- 02Advanced_Ruby_Collections/sandbox.rb
+++ 02Advanced_Ruby_Collections/sandbox.rb
@@ -4,16 +4,42 @@ array = [
   {d: 4, e: 5, f: 6}
 ]
 
+=begin
+PROBLEM: 11:24
+input: array
+  - each element is a hash
+output: new array
+  - same number of elements as input array
+  - each hash element has all values incremented by 1
+
+DATA STRUCURES
+- needs:
+  - access each individual hash
+  - access each key and value in each hash
+  - increment all values by 1
+  - avoid modifying input array
+
+- input: array
+  - new array
+    - new hashes
+- output: array
+
+ALGORITHM
+- transform input array
+  - for each hash element:
+    - create a new hash where keys are the same and each value is incremented by 1
+- return the transformed array
+=end
+
+p array
+puts
+
 # incremented = array.map do |hash|
 #   hash.map { |key, value| [key, value + 1] }.to_h
 # end
 
 incremented = array.map do |hash|
-  new_hash = {}
-  hash.each do |key, value|
-    new_hash[key] = value + 1
-  end
-  new_hash
+  hash.keys.zip(hash.values.map(&:next)).to_h
 end
 
 p array
