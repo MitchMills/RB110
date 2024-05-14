572a9c20e630e2eec950e80415aeeffddebee02f
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-14 16:57:02 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,22 +1,12 @@
 LOWERCASE = ('a'..'z').to_a
 UPPERCASE = ('A'..'Z').to_a
-ALL_LETTERS = LOWERCASE + UPPERCASE
+CONVERTER = LOWERCASE + UPPERCASE + LOWERCASE
 
-def word_cap(string)
-  string.split.map do |word|
-    word.chars.map.with_index { |char, index| change_case(char, index) }.join
-  end.join(' ')
+def swapcase(string)
+  string.chars.map do |char|
+    CONVERTER.include?(char) ? CONVERTER[CONVERTER.index(char) + 26] : char
+  end.join
 end
 
-def change_case(char, index)
-  if ALL_LETTERS.include?(char)
-    char_index = LOWERCASE.index(char) || UPPERCASE.index(char)
-    index == 0 ? UPPERCASE[char_index] : LOWERCASE[char_index]
-  else
-    char
-  end
-end
-
-p word_cap('four score and seven') == 'Four Score And Seven'
-p word_cap('the javaScript language') == 'The Javascript Language'
-p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+p swapcase('PascalCase') == 'pASCALcASE'
+p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
