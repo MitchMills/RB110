4586e9c264c0162bc508fb3939491605206df4bb
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-16 07:39:20 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -19,7 +19,7 @@
 #####
 UPPERCASE = ('A'..'Z').to_a
 LOWERCASE = ('a'..'z').to_a
-ALL_LETTERS = UPPERCASE + LOWERCASE
+ALL_LETTERS = UPPERCASE + LOWERCASE + UPPERCASE
 LETTER_SWAPPER = UPPERCASE + LOWERCASE.reverse
 
 def swapcase(string)
@@ -28,27 +28,53 @@ def swapcase(string)
   end.join
 end
 
-def change_case(char)
-  char_idx = ALL_LETTERS.index(char)
-  char_idx < 26 ? char_idx += 26 : char_idx -= 26
-  ALL_LETTERS[char_idx]
-end
+# def change_case(char)
+#   char_idx = ALL_LETTERS.index(char)
+#   ALL_LETTERS[char_idx + 26]
+# end
+
+# def change_case(char)
+#   char_idx = ALL_LETTERS.index(char)
+#   char_idx < 26 ? char_idx += 26 : char_idx -= 26
+#   ALL_LETTERS[char_idx]
+# end
+
+# def change_case(char)
+#   char_idx = UPPERCASE.index(char) || LOWERCASE.index(char)
+#   UPPERCASE.include?(char) ? LOWERCASE[char_idx] : UPPERCASE[char_idx]
+# end
+
+# def change_case(char)
+#   char_idx = UPPERCASE.index(char) || LOWERCASE.index(char)
+#   char_idx += 26 if UPPERCASE.include?(char)
+#   ALL_LETTERS[char_idx]
+# end
+
+# def change_case(char)
+#   char_idx = UPPERCASE.index(char) || LOWERCASE.index(char)
+#   swap_idx = UPPERCASE.include?(char) ? -(char_idx + 1) : char_idx
+#   LETTER_SWAPPER[swap_idx]
+# end
 
 def change_case(char)
-  char_idx = UPPERCASE.index(char) || LOWERCASE.index(char)
-  UPPERCASE.include?(char) ? LOWERCASE[char_idx] : UPPERCASE[char_idx]
-end
-
-def change_case(char)
-  char_idx = UPPERCASE.index(char) || LOWERCASE.index(char)
-  UPPERCASE.include?(char) ? ALL_LETTERS[char_idx + 26] : ALL_LETTERS[char_idx]
-end
-
-def change_case(char)
-  char_idx = UPPERCASE.index(char) || LOWERCASE.index(char)
-  swap_idx = UPPERCASE.include?(char) ? -(char_idx + 1) : char_idx
+  swap_idx = 51 - LETTER_SWAPPER.index(char)
   LETTER_SWAPPER[swap_idx]
 end
 
+# p LETTER_SWAPPER.index('A')
+# p LETTER_SWAPPER.index('a')
+# p LETTER_SWAPPER.index('M')
+# p LETTER_SWAPPER.index('m')
+# p LETTER_SWAPPER.index('Z')
+# p LETTER_SWAPPER.index('z')
+
+# p LETTER_SWAPPER[51 - LETTER_SWAPPER.index('A')]
+# p LETTER_SWAPPER[51 - LETTER_SWAPPER.index('a')]
+# p LETTER_SWAPPER[51 - LETTER_SWAPPER.index('M')]
+# p LETTER_SWAPPER[51 - LETTER_SWAPPER.index('m')]
+# p LETTER_SWAPPER[51 - LETTER_SWAPPER.index('Z')]
+# p LETTER_SWAPPER[51 - LETTER_SWAPPER.index('z')]
+
 p swapcase('PascalCase') == 'pASCALcASE'
 p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
+p swapcase(('A'..'Z').to_a.join)
