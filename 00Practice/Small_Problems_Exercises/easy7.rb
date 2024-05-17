a387d5de404be2bd6664ed4a5ea4561f5fd5d851
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-17 08:10:01 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/easy7.rb
+++ 00Practice/Small_Problems_Exercises/easy7.rb
@@ -1,3 +1,6 @@
+### 8.3
+
+
 ### 7.3 MULTIPLICATIVE
 =begin
 Write a method that takes an Array of integers as input, multiplies all the numbers together, divides the result by the number of entries in the Array, and then prints the result rounded to 3 decimal places. Assume the array is non-empty.
@@ -36,18 +39,15 @@ ALGORITHM
 #   format('%.3f', result)
 # end
 
-def show_multiplicative_average(array)
-  average = array.inject(:*).fdiv(array.size)
-  puts "The result is #{format('%.3f', average)}"
-end
+# def show_multiplicative_average(array)
+#   average = array.inject(:*).fdiv(array.size)
+#   formatted_average = format('%.3f', average)
+#   puts "The result is #{formatted_average}"
+# end
 
-show_multiplicative_average([3, 5]) #== 'The result is 7.500'
-show_multiplicative_average([6]) #== 'The result is 6.000'
-show_multiplicative_average([2, 5, 7, 11, 13, 17]) #== 'The result is 28361.667'
-
-
-
-
+# show_multiplicative_average([3, 5]) #== 'The result is 7.500'
+# show_multiplicative_average([6]) #== 'The result is 6.000'
+# show_multiplicative_average([2, 5, 7, 11, 13, 17]) #== 'The result is 28361.667'
 
 ### 6.3 STAGGER II
 =begin
