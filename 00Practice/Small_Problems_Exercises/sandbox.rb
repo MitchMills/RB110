63e1e7dc507392fa22875ecda5db9cc41518339e
Mitch Mills <mitchmillsgcp@gmail.com>
2024-05-27 10:58:05 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,9 +1,8 @@
-def sequence1(size, multiple)
-  return Array.new(size, multiple) if multiple == 0
-  multiple.step(by: multiple, to: multiple * size).to_a
-end
+GRADE_RANGES = [(90..100), (80..89), (70..79), (60..69), (0..59)]
+GRADES = %w(A B C D F)
+GRADE_BOOK = GRADE_RANGES.zip(GRADES).to_h
 
-def sequence2(size, multiple)
-  return Array.new(size, multiple) if multiple == 0
-  Array((multiple..(multiple * size)).step(multiple))
+def get_grade(score1, score2, score3)
+  average = [score1, score2, score3].sum / 3.0
+  GRADE_BOOK.each { |range, grade| return grade if range.include?(average) }
 end
