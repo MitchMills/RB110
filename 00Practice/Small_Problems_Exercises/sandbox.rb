7d5f31e30630dcc8cad337848a2fce6a02fb5b37
Mitch Mills <mitchmillsgcp@gmail.com>
2024-06-07 17:55:23 -0700

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,10 +1,33 @@
-NUMBERS = %w(zero one two three four five six seven eight nine)
+MAX_FEATURED_NUMBER = 9_876_543_201
 
-def word_to_digit(words)
-  words.split.map do |word|
-    NUMBERS.each_with_index do |number, index|
-      word[number] = index.to_s if word.include?(number)
-    end
-    word
-  end.join(' ')
+def featured(number)
+  return error_message if number >= MAX_FEATURED_NUMBER
+  starting_number = find_next_odd_multiple_of_seven(number + 1)
+  next_featured_number(starting_number)
+end
+
+def error_message
+  'There is no possible number that fulfills those requirements'
+end
+
+def find_next_odd_multiple_of_seven(number)
+  number += 1 until number.odd? && number % 7 == 0
+  number
+end
+
+def next_featured_number(starting_number)
+  (starting_number...MAX_FEATURED_NUMBER).step(14).find do |number|
+    unique_digits?(number)
+  end
+end
+
+def unique_digits?(number)
+  seen_digits = Array.new(10, false)
+  loop do
+    number, current_digit = number.divmod(10)
+    return false if seen_digits[current_digit]
+    seen_digits[current_digit] = true
+    break if number == 0
+  end
+  true
 end
