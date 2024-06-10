3bbc072abd060946cb49f1240db79f184a25bf85
Mitch Mills <mitchmillsgcp@gmail.com>
2024-06-10 06:39:31 -0700

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,8 +1,15 @@
-def smaller_than(array)
-  array.map { |number| less_thans(number, array) }
+uppercase = ('A'..'Z').to_a * 2
+lowercase = ('a'..'z').to_a * 2
+ALPHABET = uppercase + lowercase
+ALPHABET_LENGTH = 26
+SHIFT_SIZE = -23
+
+def letter_changes(string)
+  string.chars.map { |char| ALPHABET.include?(char) ? shift(char) : char }.join
 end
 
-def less_thans(number, array)
-  unique_values = array.uniq
-  unique_values.select { |value| number > value }.size
+def shift(character)
+  shift_size = SHIFT_SIZE % ALPHABET_LENGTH
+  new_index = ALPHABET.index(character) + shift_size
+  ALPHABET[new_index]
 end
