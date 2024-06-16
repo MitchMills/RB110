df2339a2c8db6abad75169cf3880a68858359dd4
Mitch Mills <mitchmillsgcp@gmail.com>
2024-06-16 17:12:54 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/medium2.rb
+++ 00Practice/Small_Problems_Exercises/medium2.rb
@@ -44,11 +44,11 @@ def triple_double(number1, number2)
   sequences1 = get_sequences(number1, 3)
   sequences2 = get_sequences(number2, 2)
 
-  triples = get_multiples(sequences1, 3)
-  doubles = get_multiples(sequences2, 2)
+  triples = get_multiples(sequences1)
+  doubles = get_multiples(sequences2)
 
-  unique_digits1 = triples.map { |triple| triple[0] }
-  unique_digits2 = doubles.map { |double| double[0] }
+  unique_digits1 = get_unique_digits(triples)
+  unique_digits2 = get_unique_digits(doubles)
 
   unique_digits1.any? { |digit| unique_digits2.include?(digit) }
 end
@@ -58,18 +58,22 @@ def get_sequences(number, target_size)
   (0..last_index).map { |start_index| number.to_s[start_index, target_size] }
 end
 
-def get_multiples(sequences, target_size)
+def get_multiples(sequences)
   sequences.select do |sequence|
     digits = sequence.chars
     digits.all? { |digit| digit == digits[0] }
   end
 end
 
-p triple_double(12345, 12345) #== false
-p triple_double(1222345, 122345) #== true
-p triple_double(1222345, 123345) #== false
-p triple_double(666789, 12345666) #== true
-p triple_double(451999277, 41177722899) #== true
+def get_unique_digits(multiples)
+  multiples.map { |multiple| multiple.chr }
+end
+
+p triple_double(12345, 12345) == false
+p triple_double(1222345, 122345) == true
+p triple_double(1222345, 123345) == false
+p triple_double(666789, 12345666) == true
+p triple_double(451999277, 41177722899) == true
 
 ### SORTING
 =begin
--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,26 +1,32 @@
 def triple_double(number1, number2)
-  sequences1 = get_sequences(number1, 3)
-  sequences2 = get_sequences(number2, 2)
-
-  triples = get_multiples(sequences1)
-  doubles = get_multiples(sequences2)
-
-  unique_digits1 = triples.map { |triple| triple[0] }
-  unique_digits2 = doubles.map { |double| double[0] }
-
-  unique_digits1.any? { |digit| unique_digits2.include?(digit) }
+  sequences = get_sequences([[number1, 3], [number2, 2]])
+  multiples = get_multiples(sequences)
+  triple_double?(multiples)
 end
 
-def get_sequences(number, target_size)
-  last_index = number.to_s.size - target_size
-  (0..last_index).map { |start_index| number.to_s[start_index, target_size] }
-end
-
-def get_multiples(sequences)
-  sequences.select do |sequence|
-    digits = sequence.chars
-    digits.all? { |digit| digit == digits[0] }
+def get_sequences(numbers_array)
+  numbers_array.map do |number, target_size|
+    last_index = number.to_s.size - target_size
+    (0..last_index).map { |start_index| number.to_s[start_index, target_size] }
   end
+end
+
+def get_multiples(sequences_array)
+  sequences_array.map do |sequences|
+    sequences.select do |sequence|
+      digits = sequence.chars
+      digits.all? { |digit| digit == digits.first }
+    end
+  end
+end
+
+def triple_double?(multiples)
+  unique_digits = get_unique_digits(multiples)
+  unique_digits.first.any? { |digit| unique_digits.last.include?(digit) }
+end
+
+def get_unique_digits(multiples_array)
+  multiples_array.map { |multiples| multiples.map(&:chr) }
 end
 
 p triple_double(12345, 12345) == false
