39761bf5aff80fa2da1a8f722009678d1f45f33c
Mitch Mills <mitchmillsgcp@gmail.com>
2024-06-17 17:08:40 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,62 +1,11 @@
-def longest(string)
-  all_substrings = get_all_substrings(string)
-  alphabetical_substrings = get_alphabetical_substrings(all_substrings)
-  alphabetical_substrings.max_by(&:size)
+def seven_eleven(number)
+  (1...number).filter_map { |num| num if num % 7 == 0 || num % 11 == 0 }.sum
 end
 
-def get_all_substrings(string)
-  all_substrings = []
-  last_index = string.size - 1
-
-  (0..last_index).each do |start_index|
-    max_length = string.size - start_index
-
-    (1..max_length).each do |length|
-      all_substrings << string[start_index, length]
-    end
-  end
-
-  all_substrings
-end
-
-def get_alphabetical_substrings(substrings)
-  substrings.select { |substring| alphabetical?(substring) }
-end
-
-def alphabetical?(string)
-  string.chars == string.chars.sort
-end
-
-#####
-# def longest(string)
-#   substrings = get_substrings(string)
-#   substrings.max_by(&:size)
-# end
-
-# def get_substrings(string)
-#   substrings = []
-#   last_index = string.size - 1
-
-#   (0..last_index).each do |start_index|
-#     max_size = string.size - start_index
-#     (1..max_size).each do |length|
-#       substring = string[start_index, length]
-#       substrings << substring if alphabetical?(substring)
-#     end
-#   end
-#   substrings
-# end
-
-# def alphabetical?(string)
-#   string.chars == string.chars.sort
-# end
-
-p longest('abc') == 'abc'
-p longest('abcace') == 'abc'
-p longest('asd') == 'as'
-p longest('nab') == 'ab'
-p longest('abcdeapbcdef') == 'abcde'
-p longest('asdfaaaabbbbcttavvfffffdf') == 'aaaabbbbctt'
-p longest('asdfbyfgiklag') == 'fgikl'
-p longest('z') == 'z'
-p longest('zyba') == 'z'
+p seven_eleven(10) == 7
+p seven_eleven(11) == 7
+p seven_eleven(12) == 18
+p seven_eleven(25) == 75
+p seven_eleven(100) == 1153
+p seven_eleven(0) == 0
+p seven_eleven(-100) == 0
