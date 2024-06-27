14b3c5cdcbe374b90dbd890855c14bf67f0202c3
Mitch Mills <mitchmillsgcp@gmail.com>
2024-06-26 19:52:23 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -106,19 +106,26 @@
 ##### first optimization
 def bubble_sort!(list)
   no_swaps = false
-  until no_swaps
-    no_swaps = one_pass(list, no_swaps)
+
+  (1..list.size).each do |offset|
+    break if no_swaps
+    no_swaps = one_pass(list, offset, no_swaps)
   end
+
+  nil
 end
 
-def one_pass(list, no_swaps)
+def one_pass(list, offset, no_swaps)
   no_swaps = true
-  list.each_cons(2).with_index do |(element1, element2), index|
-    if element1 > element2
-      swap_elements!(list, index)
+  last_index = list.size - offset
+
+  (0..last_index).each_cons(2) do |index1, index2|
+    if list[index1] > list[index2]
+      swap_elements!(list, index1)
       no_swaps = false
     end
   end
+
   no_swaps
 end
 
@@ -127,33 +134,14 @@ def swap_elements!(list, index)
 end
 
 ##### second optimization
-# def bubble_sort!(list)
-#   last_index = list.size -1
 
-#   until last_index.zero?
-#     last_index = one_pass(list, last_index)
-#   end
-#   list
-# end
-
-# def sorted?(list, last_index)
-#   (0..last_index).each_cons(2).all? { |idx1, idx2| list[idx1] <= list[idx2] }
-# end
-
-# def one_pass(list, offset)
-#   (0..(list.size - offset)).each_cons(2) do |index1, index2|
-#     if list[index1] > list[index2]
-#       swap_elements!(list, index1)
-#       last_swap = index1
-#     end
-#   end
-# end
-
-# def swap_elements!(list, index)
-#   list[index], list[index + 1] = list[index + 1], list[index]
-# end
 
 ##### TESTS
+array = [1, 2, 3, 4, 5]
+p bubble_sort!(array)
+p array
+puts
+
 array1 = [5, 3]
 bubble_sort!(array1)
 p array1 #== [3, 5]
@@ -169,6 +157,6 @@ bubble_sort!(array3)
 p array3 #== %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
 puts
 
-array4 = (0..9).to_a.reverse
+array4 = (0..9).to_a.reverse + (0..9).to_a
 bubble_sort!(array4)
 p array4
