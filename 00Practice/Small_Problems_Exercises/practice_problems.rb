27416129a01d5d87d8d55830850f4387ce832b78
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-01 05:44:29 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/practice_problems.rb
+++ 00Practice/Small_Problems_Exercises/practice_problems.rb
@@ -1,3 +1,10 @@
+### SIX
+=begin
+
+=end
+
+
+
 ### FIVE
 =begin
 Create a method that takes a string argument and returns the character that occurs most often in the string. If there are multiple characters with the same greatest frequency, return the one that appears first in the string. When counting characters, consider uppercase and lowercase versions to be the same.
@@ -32,21 +39,21 @@ ALGORITHM
   - return the earliest character with the highest number of occurences
 =end
 
-def most_common_char(string)
-  all_downcase = string.downcase
-  all_downcase.chars.max_by { |char| all_downcase.count(char) }
-end
+# def most_common_char(string)
+#   lowercase = string.downcase
+#   lowercase.chars.max_by { |char| lowercase.count(char) }
+# end
 
-p most_common_char('Hello World') == 'l'
-p most_common_char('Mississippi') == 'i'
-p most_common_char('Happy birthday!') == 'h'
-p most_common_char('aaaaaAAAA') == 'a'
+# p most_common_char('Hello World') == 'l'
+# p most_common_char('Mississippi') == 'i'
+# p most_common_char('Happy birthday!') == 'h'
+# p most_common_char('aaaaaAAAA') == 'a'
 
-my_str = 'Peter Piper picked a peck of pickled peppers.'
-p most_common_char(my_str) == 'p'
+# my_str = 'Peter Piper picked a peck of pickled peppers.'
+# p most_common_char(my_str) == 'p'
 
-my_str = 'Peter Piper repicked a peck of repickled peppers. He did!'
-p most_common_char(my_str) == 'e'
+# my_str = 'Peter Piper repicked a peck of repickled peppers. He did!'
+# p most_common_char(my_str) == 'e'
 
 ### FOUR
 =begin
