fcebbf183763f8ff14c57f18b5b9178b0db74d1c
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-02 09:05:48 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,30 +1,34 @@
-def longest_vowel_substring(string)
-  substrings = get_substrings(string)
-  vowel_substrings = substrings.select do |substring|
-    substring.each_char.all? { |char| 'aeiou'.include?(char) }
+def find_fibonacci_slices(array)
+  fib_slices = []
+  last_index =array.size - 1
+
+  (0..last_index).each do |start_index|
+    get_fib_slices(start_index, array, fib_slices)
   end
-  largest = vowel_substrings.max_by(&:size) || ''
-  largest.size
+
+  fib_slices
 end
 
-def get_substrings(string)
-  substrings = []
-  final_index = string.size - 1
+def get_fib_slices(start_index, source_array, results_array)
+  max_size = source_array.size - start_index
 
-  (0..final_index).each do |start_index|
-    max_length = string.size - start_index
-
-    (1..max_length).each do |length|
-      substrings << string[start_index, length]
-    end
+  (3..max_size).each do |length|
+    current_array = source_array[start_index, length]
+    results_array << current_array if fibonacci_slice?(current_array)
   end
-  substrings
 end
 
-p longest_vowel_substring('cwm') == 0
-p longest_vowel_substring('many') == 1
-p longest_vowel_substring('launchschoolstudents') == 2
-p longest_vowel_substring('eau') == 3
-p longest_vowel_substring('beauteous') == 3
-p longest_vowel_substring('sequoia') == 4
-p longest_vowel_substring('miaoued') == 5
+def fibonacci_slice?(array)
+  final_index = array.size - 3
+
+  (0..final_index).all? do |index|
+    current_three_elements = array[index, 3]
+    current_three_elements.take(2).sum == current_three_elements.last
+  end
+end
+
+puts find_fibonacci_slices([1, 1, 2, 3, 5, 8]) == [[1, 1, 2], [1, 1, 2, 3], [1, 1, 2, 3, 5], [1, 1, 2, 3, 5, 8], [1, 2, 3], [1, 2, 3, 5], [1, 2, 3, 5, 8], [2, 3, 5], [2, 3, 5, 8], [3, 5, 8]]
+puts find_fibonacci_slices([2, 4, 7, 11, 18]) == [[4, 7, 11], [4, 7, 11, 18], [7, 11, 18]]
+puts find_fibonacci_slices([5, 5, 10, 15, 24, 40]) == [[5, 5, 10], [5, 5, 10, 15], [5, 10, 15]]
+puts find_fibonacci_slices([1, 2, 4, 6, 10, 16]) == [[2, 4, 6], [2, 4, 6, 10], [2, 4, 6, 10, 16], [4, 6, 10], [4, 6, 10, 16], [6, 10, 16]]
+puts find_fibonacci_slices([10, 22, 33, 43, 56]) == []
