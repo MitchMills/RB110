fe8cb5663804209f1b29ef08a2f16492487baa4f
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-07 11:12:40 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -5,18 +5,11 @@ end
 def bouncy?(number)
   digits = number.digits.reverse
   pairs = digits.each_cons(2).to_a
+
   [:<, :>].all? do |comparator|
     pairs.any? { |pair| pair.inject(comparator) }
   end
 end
-
-# def ascending?(pairs)
-#   pairs.any? { |pair| pair.first < pair.last }
-# end
-
-# def descending?(pairs)
-#   pairs.any? { |pair| pair.first > pair.last }
-# end
 
 p bouncy_count([]) == 0
 p bouncy_count([11, 0, 345, 21]) == 0
