36be333637be94dd7374be30dcad0e6def076126
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-08 07:30:02 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,45 +1,39 @@
 =begin
-ALGORITHM
-- Consider every possible starting index
-  - create an empty array to hold substrings: `substrings`
-  - set a variable to the minimum length substring: `min_length`
-  - set a variable to the last index to visit: `last_index`
-    - string length - minimum length
-  - create a range to represent starting indexes: `start_indexes`
-    - 0 up to `last_index`
+ALGORITH
+- Consider every possible length of substring
+  - set a variable to the minimum length of substrings sought: `min_length`
+  - set a variable to maximum length of substrings sought: `max_length`
+  - create a range to represent possible lengths: (min_length..max_length)
 
-- From each possible starting index, consider every possible length
-  - for every start index in `start_indexes`
-    - set a variable for maximum length of the substring from that index: `max_length`
-      - string length - current starting index
-    - create a range to represent substrings lengths: `lengths`
-      - `min-length` to `max-length`
+- For each length, get a substring of that length from every possible start index
+  - transform lengths range
+    - each length becomes an array of all possible substrings of that length
+      - convert input string into an array of individual characters
+      - create all possible subarrays of current length
+      - convert each subarray into a string
+    - flatten the resulting array so it isn't nested
 
-- Add a substring from current starting index, of current length, to return array
-    - for every length in `lengths`
-      - create current substring: string[start_index, length]
-      - add current substring to `substrings` (if it meets criteria)
-
-- return `substrings`
+- Return the array of substrings
 =end
 
+# def get_all_substrings(string)
+#   substrings = []
+  # min_length = 2
+  # max_length = string.size
+  # lengths = (min_length..max_length)
+
+#   lengths.each do |length|
+#     substrings += string.chars.each_cons(length).map(&:join)
+#   end
+#   substrings
+# end
+
 def get_all_substrings(string)
-  substrings = []
   min_length = 2
-  last_index = string.size - min_length
-  start_indexes = (0..last_index)
+  max_length = string.size
+  lengths = (min_length..max_length)
 
-  start_indexes.each do |start_index|
-    max_length = string.size - start_index
-    lengths = (min_length..max_length)
-
-    lengths.each do |length|
-      substring = string[start_index, length]
-      substrings << substring if true
-    end
-  end
-
-  substrings
+  lengths.flat_map { |length| string.chars.each_cons(length).map(&:join) }
 end
 
 test_string = '12345'
