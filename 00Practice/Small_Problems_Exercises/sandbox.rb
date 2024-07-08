c01544efcb1d9e7cce4e5743ee3a73bdc805febe
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-08 07:10:50 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,17 +1,46 @@
-def bouncy_count(numbers)
-  numbers.count { |number| bouncy?(number) }
-end
+=begin
+ALGORITHM
+- Consider every possible starting index
+  - create an empty array to hold substrings: `substrings`
+  - set a variable to the minimum length substring: `min_length`
+  - set a variable to the last index to visit: `last_index`
+    - string length - minimum length
+  - create a range to represent starting indexes: `start_indexes`
+    - 0 up to `last_index`
 
-def bouncy?(number)
-  digits = number.digits.reverse
-  pairs = digits.each_cons(2).to_a
+- From each possible starting index, consider every possible length
+  - for every start index in `start_indexes`
+    - set a variable for maximum length of the substring from that index: `max_length`
+      - string length - current starting index
+    - create a range to represent substrings lengths: `lengths`
+      - `min-length` to `max-length`
 
-  [:<, :>].all? do |comparator|
-    pairs.any? { |pair| pair.inject(comparator) }
+- Add a substring from current starting index, of current length, to return array
+    - for every length in `lengths`
+      - create current substring: string[start_index, length]
+      - add current substring to `substrings` (if it meets criteria)
+
+- return `substrings`
+=end
+
+def get_all_substrings(string)
+  substrings = []
+  min_length = 2
+  last_index = string.size - min_length
+  start_indexes = (0..last_index)
+
+  start_indexes.each do |start_index|
+    max_length = string.size - start_index
+    lengths = (min_length..max_length)
+
+    lengths.each do |length|
+      substring = string[start_index, length]
+      substrings << substring if true
+    end
   end
+
+  substrings
 end
 
-p bouncy_count([]) == 0
-p bouncy_count([11, 0, 345, 21]) == 0
-p bouncy_count([121, 4114]) == 2
-p bouncy_count([176, 442, 80701644]) == 2
+test_string = '12345'
+p get_all_substrings(test_string)
