97909cdd59b2196429b478072e874f420c7ab334
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-15 13:34:44 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,22 +1,35 @@
-UPPERCASE = ('A'..'Z').to_a
+def longest_alternating_subarray(numbers)
+  subarrays = get_subarrays(numbers)
+  alternating = subarrays.select { |subarray| alternating?(subarray) }
+  alternating.max_by(&:size) || []
+end
 
-def decode(strings)
-  strings.map do |string|
-    uppercase_indexes = get_uppercase_indexes(string)
-    uppercase_indexes.size < 2 ? 0 : index_difference(uppercase_indexes)
+def get_subarrays(array)
+  subarrays = []
+  min_length = 2
+  last_index = array.size - min_length
+  start_indexes = (0..last_index)
+
+  start_indexes.each do |start_index|
+    max_length = array.size - start_index
+    lengths = (min_length..max_length)
+
+    lengths.each do |length|
+      subarrays << array[start_index, length]
+    end
+  end
+  subarrays
+end
+
+def alternating?(subarray)
+  pairs = subarray.each_cons(2).to_a
+  pairs.all? do |pair|
+    pair.first.odd? ? pair.last.even? : pair.last.odd?
   end
 end
 
-def get_uppercase_indexes(string)
-  indexes = string.chars.each_index.to_a
-  indexes.select { |index| UPPERCASE.include?(string[index]) }
-end
-
-def index_difference(array)
-  array.take(2).inject(:-).abs - 1
-end
-
-p decode(['ZoL', 'heLlo', 'XX']) == [1, 0, 0]
-p decode(['foUrsCoreAnd', 'seven', '']) == [2, 0, 0]
-p decode(['lucYintheskyWith', 'dIaMonDs']) == [8, 1]
-p decode([]) == []
+p longest_alternating_subarray([1, 2, 3, 4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+p longest_alternating_subarray([2, 4, 6, 8]) == []
+p longest_alternating_subarray([1, 3, 5, 7]) == []
+p longest_alternating_subarray([1, 1, 3, 7, 8, 5]) == [7, 8, 5]
+p longest_alternating_subarray([4, 6, 7, 12, 11, 9, 17]) == [6, 7, 12, 11]
