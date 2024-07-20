618f37bcf634efbf714bdb28c69a75ba0e931028
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-20 07:32:08 -0500

    RB120 Notes

--- 00Practice/Small_Problems_Exercises/advanced1.rb
+++ 00Practice/Small_Problems_Exercises/advanced1.rb
@@ -1,4 +1,87 @@
-### 2 SEEING START
+### MATRIX
+=begin
+A 3 x 3 matrix can be represented by an Array of Arrays in which the main Array and all of the sub-Arrays has 3 elements. For example:
+
+1  5  8
+4  7  2
+3  9  6
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+The transpose of a 3 x 3 matrix is the matrix that results from exchanging the columns and rows of the original matrix. For example, the transposition of the array shown above is:
+
+1  4  3
+5  7  9
+8  2  6
+
+Write a method that takes a 3 x 3 matrix in Array of Arrays format and returns the transpose of the original matrix. Note that there is a Array#transpose method that does this -- you may not use it for this exercise. You also are not allowed to use the Matrix class from the standard library. Your task is to do this yourself.
+
+Take care not to modify the original matrix: you must produce a new matrix and leave the original matrix unchanged.
+
+PROBLEM 6:56
+input: array
+  - contains three subarrays
+    - each subarray contains three elements
+output: new array
+  - transpose of input array
+    - rows become columns
+      - each subarray in a particular index position
+        - contains all elements at that index postion from each subarray
+
+EXAMPLES
+
+DATA STRUCTURES
+needs:
+  - way to access each subarray
+  - way to access each element in each subarray
+  - new array with subarrays
+
+start: array
+  - new empty array with three empty subarrays
+finish: new array
+
+ALGORITHM
+- create an empty array with three empty subarrays
+- for each subarray in the input array
+  - for each element in that subarray, with its index
+    - add the element at that index to the new subarray with that index
+- return the new array
+=end
+
+# def transpose(matrix)
+#   new_matrix = Array.new(3) { [] }
+#   matrix.each do |subarray|
+#     subarray.each_with_index do |element, index|
+#       new_matrix[index] << element
+#     end
+#   end
+#   new_matrix
+# end
+
+def transpose(matrix)
+  matrix.map.with_index do |subarray, row_index|
+    subarray.map.with_index do |element, column_index|
+      matrix[column_index][row_index]
+    end
+  end
+end
+
+matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix #== [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p matrix == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+### 2 SEEING STARs
 =begin
 Write a method that displays an 8-pointed star in an nxn grid, where n is an odd integer that is supplied as an argument to the method. The smallest such star you need to handle is a 7x7 grid.
 
@@ -37,40 +120,40 @@ second star:  3
 last star:   6 5 4
 =end
 
-def star(size)
-  all_rows = get_all_rows(size)
-  puts all_rows
-end
+# def star(size)
+#   all_rows = get_all_rows(size)
+#   puts all_rows
+# end
 
-def get_all_rows(star_size)
-  top_rows = get_top_rows(star_size)
-  middle_row = ['*' * star_size]
-  bottom_rows = top_rows.reverse
-  top_rows + middle_row + bottom_rows
-end
+# def get_all_rows(star_size)
+#   top_rows = get_top_rows(star_size)
+#   middle_row = ['*' * star_size]
+#   bottom_rows = top_rows.reverse
+#   top_rows + middle_row + bottom_rows
+# end
 
-def get_top_rows(star_size)
-  number_of_rows = star_size / 2
-  last_index = star_size - 1
+# def get_top_rows(star_size)
+#   number_of_rows = star_size / 2
+#   last_index = star_size - 1
 
-  number_of_rows.times.map do |row_number|
-    star_indexes = get_star_indexes(row_number, last_index)
-    get_one_row(star_size, star_indexes)
-  end
-end
+#   number_of_rows.times.map do |row_number|
+#     star_indexes = get_star_indexes(row_number, last_index)
+#     get_one_row(star_size, star_indexes)
+#   end
+# end
 
-def get_star_indexes(row_number, last_index)
-  first_star_index = row_number
-  middle_star_index = last_index / 2
-  last_star_index = last_index - first_star_index
-  [first_star_index, middle_star_index, last_star_index]
-end
+# def get_star_indexes(row_number, last_index)
+#   first_star_index = row_number
+#   middle_star_index = last_index / 2
+#   last_star_index = last_index - first_star_index
+#   [first_star_index, middle_star_index, last_star_index]
+# end
 
-def get_one_row(star_size, star_indexes)
-  star_size.times.map { |index| star_indexes.include?(index) ? '*' : ' ' }.join
-end
+# def get_one_row(star_size, star_indexes)
+#   star_size.times.map { |index| star_indexes.include?(index) ? '*' : ' ' }.join
+# end
 
-star(7)
+# star(7)
 
 ### 3 MATRIX, MUTATING
 =begin
--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,34 +1,28 @@
-def star(size)
-  all_rows = get_all_rows(size)
-  puts all_rows
+def transpose(matrix)
+  new_matrix = Array.new(3) { [] }
+  matrix.each do |subarray|
+    subarray.each_with_index do |element, index|
+      new_matrix[index] << element
+    end
+  end
+  new_matrix
 end
 
-def get_all_rows(star_size)
-  top_rows = get_top_rows(star_size)
-  middle_row = ['*' * star_size]
-  bottom_rows = top_rows.reverse
-  top_rows + middle_row + bottom_rows
-end
-
-def get_top_rows(star_size)
-  number_of_rows = star_size / 2
-  last_index = star_size - 1
-
-  number_of_rows.times.map do |row_number|
-    star_indexes = get_star_indexes(row_number, last_index)
-    get_one_row(star_size, star_indexes)
+def transpose1(matrix)
+  matrix.map.with_index do |subarray, row_index|
+    subarray.each_index.map do |column_index|
+      matrix[column_index][row_index]
+    end
   end
 end
 
-def get_star_indexes(row_number, last_index)
-  first_star_index = row_number
-  middle_star_index = last_index / 2
-  last_star_index = last_index - first_star_index
-  [first_star_index, middle_star_index, last_star_index]
-end
+matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
 
-def get_one_row(star_size, star_indexes)
-  star_size.times.map { |index| star_indexes.include?(index) ? '*' : ' ' }.join
-end
+new_matrix = transpose(matrix)
 
-star(7)
+p new_matrix == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p matrix == [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
