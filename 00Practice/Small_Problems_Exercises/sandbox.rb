01eb8b54056a2588d24cde4a32b911db886d1ad8
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-27 12:22:51 -0500

    RB120 Notes

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,26 +1,25 @@
-def first_and_last_case(string)
-  multiples = get_multiples(string)
-  indexes = get_indexes(multiples, string)
-  string.chars.map.with_index do |char, index|
-    indexes.include?(index) ? char.upcase : char
-  end.join
-end
+class GoodDog
+  attr_accessor :name, :height, :weight
 
-def get_multiples(string)
-  string.chars.uniq.select { |char| string.count(char) > 1 }
-end
-
-def get_indexes(multiples, string)
-  all_indexes = multiples.map do |letter|
-    string.chars.each_index.select { |index| string[index] == letter }
+  def initialize(n, h, w)
+    self.name   = n
+    self.height = h
+    self.weight = w
   end
-  all_indexes.flat_map { |indexes| [indexes.first, indexes.last] }
+
+  def change_info(n, h, w)
+    self.name   = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
+  end
+
+  def what_is_self
+    self
+  end
+
+  puts self
 end
-
-p first_and_last_case('abc') == 'abc'
-p first_and_last_case('aaabc') == 'AaAbc'
-p first_and_last_case('aaabccbb') == 'AaABCCbB'
-
-string = 'thequickbrownfoxjumpsoverthelazydog'
-new_string = 'THEqUickbROwnfoxjUmpsoveRTHElazydOg'
-p first_and_last_case(string) == new_string
