0276746d43496d220ceb9874549edd06e74efdfa
Mitch Mills <mitchmillsgcp@gmail.com>
2024-07-29 11:27:31 -0500

    PEDAC practice

--- 00Practice/Small_Problems_Exercises/sandbox.rb
+++ 00Practice/Small_Problems_Exercises/sandbox.rb
@@ -1,107 +1,96 @@
-# def anagram_difference(string1, string2)
-#   target_anagram = []
+# FIRST AND LAST
+=begin
+Write a method that takes a string as an argument and returns a new string according to
+the following rules:
 
-#   target_letters1 = string1.chars.select { |char| string2.include?(char) }
-#   target_letters2 = string2.chars.select { |char| string1.include?(char) }
+If a letter occurs only once in the string, leave it unchanged
 
-#   target_letters1.each do |char|
-#     count1 = target_letters1.count(char)
-#     count2 = target_letters2.count(char)
-#     target_count = [count1, count2].min
-#     target_anagram << char if target_anagram.count(char) < target_count
-#   end
+If a letter occurs more than once, capitalize the first and last occurrences of that
+letter in the string
 
-#   target_size = target_anagram.size
-#   (string1.size - target_size) + (string2.size - target_size)
-# end
+The input string will only contain lowercase alphabetic characters
 
-#####
-# def anagram_difference(string1, string2)
-#   common_letters = string1.chars.select { |char| string2.include?(char) }.uniq
+PROBLEM 6:25
+input: string
+  - contains only lowercase alphabetic characters
+output: new string
+  - if a letter occurs only once in the input string
+    - leave it unchanged
+  - if a letter occurs more than once
+    - capitalize 1st and last occurrences
 
-#   tally1 = string1.chars.tally
-#   tally2 = string2.chars.tally
+EXAMPLES
 
-#   anagram_letters = ''
-#   common_letters.each do |letter|
-#     reps = [tally1[letter], tally2[letter]].min
-#     anagram_letters << (letter * reps)
-#   end
-#   anagram_length = anagram_letters.size
-#   (string1.size - anagram_length) + (string2.size - anagram_length)
-# end
+DATA STRUCTURES
+needs:
+  - way to determine which letters occur more than once: multiples
+  - way to get indexes of multiples
+  - way to get first and last indexes of multiples
+  - way to capitalize letters at those indexes
 
-#####
-# def anagram_difference(string1, string2)
-#   anagram_length = get_anagram_length(string1, string2)
-#   (string1.size - anagram_length) + (string2.size - anagram_length)
-# end
+start: string
+  - array of individual letters in input string
+  - array of unique letters
+  - array of unique letters that occur more than once: multiples
+  - nested array of indexes of each multiple
+    - subarrays are all the indexes a particular letter occurs at
+  - flat array of all first and last indexes
+  - transformed array of individual letters
+finish: new string
 
-# def get_anagram_length(string1, string2)
-#   common_letters = get_common_letters(string1, string2)
-#   letter_counts1, letter_counts2 = get_letter_counts(string1, string2)
+ALGORITHM
+- determine which letters occur more than once
+  - create an array of unique characters from the input string
+  - select unique characters that occur more than once in the input string: multiples
+- get the first and last indexes of those letters
+  - transform the multiples array
+    - each letter becomes a subarray of indexes
+     - iterate over each letter
+      - iterate over each index of the input string
+        - select indexes where the letter at that index matches the current letter
+    - transform the indexes array
+      - each subarray is the first and last index for a particular letter
+    - flatten the transformed array
+- create a new string with letters at those indexes capitalized
+  - create an array of individual characters from the input string
+  - transform this array:
+    - for each letter and its index
+      - capitalize the letter if the index is in the indexes array
+      - otherwise leave as is
+  - convert the array to a string and return it
+=end
 
-#   common_letters.map do |letter|
-#     [letter_counts1[letter], letter_counts2[letter]].min
-#   end.sum
-# end
+def first_and_last_case(string)
+  multiples = get_multiples(string)
+  indexes = get_indexes(multiples, string)
 
-# def get_common_letters(string1, string2)
-#   string1.chars.select { |char| string2.include?(char) }.uniq
-# end
+  string.chars.map.with_index do |char, index|
+    indexes.include?(index) ? char.upcase : char
+  end.join
+end
 
-# def get_letter_counts(string1, string2)
-#   [string1.chars.tally, string2.chars.tally]
-# end
+def get_multiples(string)
+  string.chars.uniq.select do |char|
+    string.count(char) > 1
+  end
+end
 
-#####
-# def anagram_difference(string1, string2)
-#   anagram_length = get_anagram_length(string1, string2)
-#   (string1.size - anagram_length) + (string2.size - anagram_length)
-# end
+def get_indexes(multiples, string)
+  all_indexes = multiples.map do |letter|
+    string.chars.each_index.select do |index|
+      string[index] == letter
+    end
+  end
 
-# def get_anagram_length(string1, string2)
-#   common_letters = string1.chars.select { |char| string2.include?(char) }.uniq
-#   letter_counts1, letter_counts2 = [string1.chars.tally, string2.chars.tally]
+  all_indexes.map do |indexes|
+    [indexes.first, indexes.last]
+  end.flatten
+end
 
-#   common_letters.map do |letter|
-#     [letter_counts1[letter], letter_counts2[letter]].min
-#   end.sum
-# end
+p first_and_last_case('abc') == 'abc'
+p first_and_last_case('aaabc') == 'AaAbc'
+p first_and_last_case('aaabccbb') == 'AaABCCbB'
 
-#####
-# def anagram_difference(string1, string2)
-#   common_letters = (string1 + string2).chars.uniq
-#   # common_letters = string1.chars | string2.chars
-#   common_letters.map do |letter|
-#     (string1.count(letter) - string2.count(letter)).abs
-#   end.sum
-# end
-
-p anagram_difference('', '') == 0
-p anagram_difference('a', '') == 1
-p anagram_difference('', 'a') == 1
-p anagram_difference('ab', 'a') == 1
-p anagram_difference('ab', 'ba') == 0
-p anagram_difference('ab', 'cd') == 4
-p anagram_difference('aab', 'a') == 2
-p anagram_difference('a', 'aab') == 2
-p anagram_difference('codewars', 'hackerrank') == 10
-
-p anagram_difference(
-  "oudvfdjvpnzuoratzfawyjvgtuymwzccpppeluaekdlvfkhclwau",
-  "trvhyfkdbdqbxmwpbvffiodwkhwjdjlynauunhxxafscwttqkkqw"
-  ) == 42
-p anagram_difference(
-  "trvhyfkdbdqbxmwpbvffiodwkhwjdjlynauunhxxafscwttqkkqw",
-  "oudvfdjvpnzuoratzfawyjvgtuymwzccpppeluaekdlvfkhclwau"
-  ) == 42
-
-p anagram_difference(
-  "fcvgqognzlzxhmtjoahpajlplfqtatuhckxpskhxiruzjirvpimrrqluhhfkkjnjeuvxzmxo",
-  "qcfhjjhkghnmanwcthnhqsuigwzashweevbegwsbetjuyfoarckmofrfcepkcafznykmrynt"
-  ) == 50
-p anagram_difference(
-  "qcfhjjhkghnmanwcthnhqsuigwzashweevbegwsbetjuyfoarckmofrfcepkcafznykmrynt",
-  "fcvgqognzlzxhmtjoahpajlplfqtatuhckxpskhxiruzjirvpimrrqluhhfkkjnjeuvxzmxo"
-  ) == 50
+string = 'thequickbrownfoxjumpsoverthelazydog'
+new_string = 'THEqUickbROwnfoxjUmpsoveRTHElazydOg'
+p first_and_last_case(string) == new_string
